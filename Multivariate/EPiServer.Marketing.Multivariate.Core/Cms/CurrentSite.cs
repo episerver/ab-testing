32cf82087ec7b645a2a42264fb57c06d478c91a2
jmasterson81 <Jason.Masterson@ektron.com>
2015-06-23 09:43:19 -0400

    Added Nuget package creation support. Updated the test to be just a data class and removed the test file for it. Added support to get the current site's connection string from the EPiServer core to pass to the DAL when that change is in place.

---
+++ Multivariate/EPiServer.Marketing.Multivariate.Core/Cms/CurrentSite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPiServer.Data.Configuration;
+
+namespace EPiServer.Marketing.Multivariate.Core.Cms
+{
+    public class CurrentSite : ICurrentSite
+    {
+        public string GetSiteDataBaseConnectionString()
+        {
+            var siteSettings = new SiteDataSettingsElement();
+            return siteSettings.ConnectionStringName;
+        }
+    }
+}
