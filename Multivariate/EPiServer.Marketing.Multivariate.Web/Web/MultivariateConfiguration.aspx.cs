c8383c9872c48725214aa63177991ba38ca7a358
Adam Pitcher <Adam.Pitcher@ektron.com>
2015-11-20 13:49:41 -0500

    Blank Settings page for Multivariate Configuration

---
+++ Multivariate/EPiServer.Marketing.Multivariate.Web/Web/MultivariateConfiguration.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EPiServer.PlugIn;
+using EPiServer.Shell.WebForms;
+
+
+namespace EPiServer.Marketing.Multivariate.Web.Web
+{
+    [GuiPlugIn(Area = PlugInArea.AdminConfigMenu, UrlFromModuleFolder = "MultivariateWeb/MultivariateConfiguration.aspx", DisplayName = "Multivariate Test Configuration")]
+    public partial class MultivariateConfiguration : WebFormsBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
