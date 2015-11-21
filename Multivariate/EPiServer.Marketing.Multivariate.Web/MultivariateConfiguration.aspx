ab07a4e145da2e02ff718d34e0f7e0fc392a7353
adam-pitcher <apitcher@apitcher.net>
2015-11-20 23:50:36 -0500

    Fixed broken page inheritance.  Admin link now loads page as it should have the first time.

---
+++ Multivariate/EPiServer.Marketing.Multivariate.Web/MultivariateConfiguration.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Language="C#" AutoEventWireup="false" CodeBehind="MultivariateConfiguration.aspx.cs" Inherits="EPiServer.Marketing.Multivariate.Web.MultivariateConfiguration" %> 
+<%@ Import Namespace="EPiServer" %>
+<%@ Register Assembly="EPiServer.UI" Namespace="EPiServer.UI.WebControls" TagPrefix="EPiServerUI" %>
+
+<asp:content contentplaceholderid="HeaderContentRegion" runat="server">
+</asp:content>
+
+<asp:content contentplaceholderid="FullRegion" runat="server">
+    </asp:content>
--- Multivariate/EPiServer.Marketing.Multivariate.Web/MultivariateConfiguration.aspx.cs
+++ Multivariate/EPiServer.Marketing.Multivariate.Web/MultivariateConfiguration.aspx.cs
@@ -3,9 +3,9 @@ using EPiServer.PlugIn;
 using EPiServer.Shell.WebForms;
 
 
-namespace EPiServer.Marketing.Multivariate.Web.Web
+namespace EPiServer.Marketing.Multivariate.Web
 {
-    [GuiPlugIn(Area = PlugInArea.AdminConfigMenu, UrlFromModuleFolder = "MultivariateWeb/MultivariateConfiguration.aspx", DisplayName = "Multivariate Test Configuration")]
+    [GuiPlugIn(Area = PlugInArea.AdminConfigMenu, UrlFromModuleFolder ="MultivariateConfiguration.aspx", DisplayName = "Multivariate Test Configuration")]
     public partial class MultivariateConfiguration : WebFormsBase
     {
         protected void Page_Load(object sender, EventArgs e)
