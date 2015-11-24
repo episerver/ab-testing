b8418ae8ab297982b51c47cf7251a300b740fdc3
adam-pitcher <apitcher@apitcher.net>
2015-11-23 21:01:01 -0500

    Initial repositories - functionally contrived save test until form issues are resolved.

---
+++ Multivariate/EPiServer.Marketing.Multivariate.Web/Repositories/IMultivariateTestRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPiServer.Marketing.Multivariate.Web.Repositories
+{
+    interface IMultivariateTestRepository
+    {
+        void CreateTest(string title, DateTime testStart, DateTime testStop);
+    }
+}
---
+++ Multivariate/EPiServer.Marketing.Multivariate.Web/Repositories/MultivariateTestRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EPiServer.Marketing.Multivariate.Web.Repositories
+{
+    class MultivariateTestRepository : IMultivariateTestRepository
+    {
+        private readonly IMultivariateTestManager _multivariateTestManager = new MultivariateTestManager();
+        private IMultivariateTest _multivariateTest = new MultivariateTest();
+
+        public void CreateTest(string title, DateTime testStart, DateTime testStop)
+        {
+            //temp fake forced create for getting a functioning create test working until asp tag woes
+            //are fixed.
+
+
+            _multivariateTest = new MultivariateTest
+            {
+                Title = $"Title{DateTime.Now.DayOfWeek}{DateTime.Now.Second}",
+                Id = Guid.NewGuid(),
+                OriginalItemId = Guid.NewGuid(),
+                VariantItemId = Guid.NewGuid(),
+                ConversionItemId = Guid.NewGuid(),
+                Owner = Security.PrincipalInfo.CurrentPrincipal.Identity.Name,
+                StartDate = DateTime.Now.AddDays(1),
+                EndDate = DateTime.Now.AddDays(5)
+            };
+
+            _multivariateTestManager.Save(_multivariateTest);
+        }
+    }
+}
