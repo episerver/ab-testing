9ff25f569e998aa1b22d5b2f53a0974a070f4c1c
adam-pitcher <apitcher@apitcher.net>
2015-12-02 23:18:46 -0500

    Create test form along with supporting view model with initial validations, start and end date validators and proper view flow.

---
+++ Multivariate/EPiServer.Marketing.Multivariate.Web/Models/MultivariateTestViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPiServer.Web;
+
+namespace EPiServer.Marketing.Multivariate.Web.Models
+{
+    public class MultivariateTestViewModel
+    {
+        [Required]
+        [Display(Name="Test Title")]
+        public string TestTitle { get; set; }
+        [Required(ErrorMessage = "A start Date and Time is required")]
+        [Display(Name="Test Start")]
+        [StartDate(ErrorMessage="Start date cannot be in the past")]
+        public DateTime TestStart { get; set; }
+        [Required(ErrorMessage = "A start Date and Time is required")]
+        [Display(Name="Test Stop")]
+        [EndDate(StartDate= "TestStart",ErrorMessage = "End date cannot be before Start Date")]
+        public DateTime TestStop { get; set; }
+        public int OriginalItemId { get; set; }
+        public int VariantItemId { get; set; }
+        public int ConversionItemId { get; set; }
+    }
+
+    
+}
