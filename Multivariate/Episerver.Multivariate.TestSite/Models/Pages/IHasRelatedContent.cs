ffa305cf2f47bc6bb41a65f7b16fb649bb4d217d
Adam Pitcher <Adam.Pitcher@ektron.com>
2015-12-08 15:49:00 -0500

    Attempting to clean up Test Pages projects.

--- Multivariate/Episerver.Multivariate.TestSite/Models/Pages/IHasRelatedContent.cs
+++
@@ -1,9 +0,0 @@
-using EPiServer.Core;
-
-namespace Episerver.Multivariate.TestSite.Models.Pages
-{
-    public interface IHasRelatedContent
-    {
-        ContentArea RelatedContentArea { get; }
-    }
-}
