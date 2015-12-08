a7b3c1eae4dad81d337d1dc12415fd0a2837274d
Adam Pitcher <Adam.Pitcher@ektron.com>
2015-12-08 14:27:30 -0500

    Changes to support distribution of API test pages. Includes Initializer for routeconfig and controller/view changes to support upcomming Core/DAL changes.

--- Multivariate/EpiServer.Multivariate.TestPages/Global.asax.cs
+++ Multivariate/EpiServer.Multivariate.TestPages/Global.asax.cs
@@ -13,18 +13,7 @@ namespace EpiServer.Multivariate.TestPages
             
             //Tip: Want to call the EPiServer API on startup? Add an initialization module instead (Add -> New Item.. -> EPiServer -> Initialization Module)
         }
-        protected override void RegisterRoutes(RouteCollection routes)
-        {
-
-            base.RegisterRoutes(routes);
-
-
-
-            routes.MapRoute(name: "AB API Testing",
-               url: "{controller}/{action}/{state}",
-               defaults: new { controller = "MultivariateApiTesting", action = "Index", state = UrlParameter.Optional });
-
-            
-        }
+        
+        
     }
 }
