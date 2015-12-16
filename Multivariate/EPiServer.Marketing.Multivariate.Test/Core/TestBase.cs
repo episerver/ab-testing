c33bb5601fc47fce2c4ec684988e74ed882561b1
Brian Petrowicz <brian.petrowicz@episerver.com>
2015-12-16 12:17:02 -0500

    2 more files missed for add

---
+++ Multivariate/EPiServer.Marketing.Multivariate.Test/Core/TestBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using EPiServer.Marketing.Multivariate.Test.Dal;
+
+namespace EPiServer.Marketing.Multivariate.Test.Core
+{
+    public class TestBase
+    {
+        public void AddObjectsToContext<T>(TestContext context, IList<T> data) where T : class
+        {
+            context.Set<T>().AddRange(data);
+        }
+
+
+    }
+}
---
+++ Multivariate/EPiServer.Marketing.Multivariate.Test/Dal/TestContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.Entity;
+using EPiServer.Marketing.Multivariate.Dal;
+using EPiServer.Marketing.Multivariate.Model;
+
+namespace EPiServer.Marketing.Multivariate.Test.Dal
+{
+    public class TestContext : DatabaseContext
+    {
+        public TestContext(DbConnection dbConnection) : base(dbConnection)
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            if (modelBuilder != null)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<MultivariateTest>();
+                modelBuilder.Entity<MultivariateTestResult>();
+                modelBuilder.Entity<Conversion>();
+                modelBuilder.Entity<Variant>();
+                modelBuilder.Entity<KeyPerformanceIndicator>();
+            }
+        }
+    }
+}
