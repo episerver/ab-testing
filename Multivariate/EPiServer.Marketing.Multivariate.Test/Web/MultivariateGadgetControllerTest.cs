b83d414b2141aafffa6d2e49f49a5681a109d10b
Herb McCarthy <Herb.McCarthy@episerver.com>
2016-01-07 14:05:55 -0500

    Added some unit tests for gadget controller

---
+++ Multivariate/EPiServer.Marketing.Multivariate.Test/Web/MultivariateGadgetControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EPiServer.ServiceLocation;
+using Moq;
+using EPiServer.Marketing.Multivariate.Web;
+using EPiServer.Marketing.Multivariate.Web.Repositories;
+using EPiServer.Marketing.Multivariate.Dal;
+
+namespace EPiServer.Marketing.Multivariate.Test.Web
+{
+    [TestClass]
+    public class MultivariateGadgetControllerTest
+    {
+        private Mock<IServiceLocator> _serviceLocator;
+        private Mock<IMultivariateTestRepository> _testRepository;
+
+        private MultivariateGadgetController GetUnitUnderTest()
+        {
+            _serviceLocator = new Mock<IServiceLocator>();
+            _testRepository = new Mock<IMultivariateTestRepository>();
+            _serviceLocator.Setup(sl => sl.GetInstance<IMultivariateTestRepository>()).Returns(_testRepository.Object);
+
+            return new MultivariateGadgetController(_serviceLocator.Object);
+        }
+
+
+        [TestMethod]
+        public void GadgetControlCallsRepositoryForIndexAction()
+        {
+            var controller = GetUnitUnderTest();
+
+            controller.Index();
+
+            // Verify the testmanager was called by the repo with the proper argument
+            _testRepository.Verify(tr=> tr.GetTestList(It.IsAny<MultivariateTestCriteria>()),
+                Times.Once, "GadgetController did not call GetTestList");
+
+        }
+    }
+}
