6633cf9b686c4425e20d26539aa117beb259798e
Adam Pitcher <Adam.Pitcher@ektron.com>
2016-01-08 16:30:12 -0500

    Beginnings of AdministrationController unit tests.

---
+++ Multivariate/EPiServer.Marketing.Multivariate.Test/Web/MultivariateAdministrationControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EPiServer.ServiceLocation;
+using Moq;
+using EPiServer.Marketing.Multivariate.Web;
+using EPiServer.Marketing.Multivariate.Web.Repositories;
+using EPiServer.Marketing.Multivariate.Dal;
+
+namespace EPiServer.Marketing.Multivariate.Test.Web
+{
+    [TestClass]
+    public class MultivariateAdministrationControllerTest
+    {
+        private Mock<IServiceLocator> _serviceLocator;
+        private Mock<IMultivariateTestRepository> _testRepository;
+
+        private MultivariateAdministrationController GetUnitUnderTest()
+        {
+            _serviceLocator = new Mock<IServiceLocator>();
+            _testRepository = new Mock<IMultivariateTestRepository>();
+            _serviceLocator.Setup(sl => sl.GetInstance<IMultivariateTestRepository>()).Returns(_testRepository.Object);
+
+            return new MultivariateAdministrationController(_serviceLocator.Object);
+        }
+
+        [TestMethod]
+        public void AdministrationController_IndexAction_CallsTestRepositoryGetTestList()
+        {
+            var controller = GetUnitUnderTest();
+
+            controller.Index();
+
+            _testRepository.Verify(x=>x.GetTestList(It.IsAny<MultivariateTestCriteria>()),
+                Times.Once,"Multivariate Administration Controller Index Did Not Properly Call Repositories GetTestList");
+        }
+
+    }
+
+    
+}
