d4acc41635e0204ebb08a8a6afb10100b472e310
Jason Masterson <jason.masterson@episerver.com>
2016-01-08 16:26:48 -0500

    MAR-86: Added a list of filters to the criteria object, filters contain a MultiVariateTestProperty and the corresponding operator for the filter.

--- Multivariate/EPiServer.Marketing.Multivariate.Model/MultivariateTestCriteria.cs
+++ Multivariate/EPiServer.Marketing.Multivariate.Model/MultivariateTestCriteria.cs
@@ -1,7 +1,60 @@
-﻿namespace EPiServer.Marketing.Multivariate.Model
+﻿using System.Collections.Generic;
+
+namespace EPiServer.Marketing.Multivariate.Model
 {
     public class MultivariateTestCriteria
     {
+        public MultivariateTestCriteria()
+        {
+            filters = new List<MultivariateTestFilter>();
+        }
 
+        private List<MultivariateTestFilter> filters;
+
+        /// <summary>
+        /// Adds the given filter to the collection of criteria filters if the property on the filter doesn't exist
+        /// If the filter exists the filter will not be added
+        /// </summary>
+        /// <param name="filter">the filter to add</param>
+        public void AddFilter(MultivariateTestFilter filter)
+        {
+            if(!filters.Exists(f => f.Property == filter.Property))
+            {
+                filters.Add(filter);
+            }
+        }
+
+        public List<MultivariateTestFilter> GetFilters()
+        {
+            return filters;
+        }
     }
+
+    public class MultivariateTestFilter
+    {
+        public MultivariateTestFilter(MultivariateTestProperty theProperty, FilterOperator theOperator)
+        {
+            Property = theProperty;
+            Operator = theOperator;
+        }
+
+        private MultivariateTestFilter() { }
+        
+        public MultivariateTestProperty Property { get; private set; }
+        public FilterOperator Operator { get; private set; }
+    }
+
+    public enum MultivariateTestProperty
+    {
+        State = 0,
+        OriginalItemId = 1,
+        VariantItemId = 2
+    }
+
+    public enum FilterOperator
+    {
+        And = 0,
+        Or = 1
+    }
+
 }
---
+++ Multivariate/EPiServer.Marketing.Multivariate.Test/Model/MultivariateTestCriteriaTests.cs
@@ -0,0 +1,32 @@
+﻿using EPiServer.Marketing.Multivariate.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace EPiServer.Marketing.Multivariate.Test.Model
+{
+    [TestClass]
+    public class MultivariateTestCriteriaTests
+    {
+        [TestMethod]
+        public void Get_And_Add_Filter_Methods_Operate_On_The_Collection()
+        {
+            var testCriteria = new MultivariateTestCriteria();
+
+            testCriteria.AddFilter(new MultivariateTestFilter(MultivariateTestProperty.State, FilterOperator.And));
+            var retFilters = testCriteria.GetFilters();
+            Assert.IsTrue(retFilters.Count == 1, "The filter was not added to the criteria object");
+        }
+
+        [TestMethod]
+        public void AddFilter_Will_Not_Add_If_The_Property_Exists()
+        {
+            var testCriteria = new MultivariateTestCriteria();
+            var dupeFilter = new MultivariateTestFilter(MultivariateTestProperty.State, FilterOperator.And);
+
+            testCriteria.AddFilter(dupeFilter);
+            testCriteria.AddFilter(dupeFilter);
+            var retFilters = testCriteria.GetFilters();
+            Assert.IsTrue(retFilters.Count == 1, "Only one filter should be added to the criteria object");
+        }
+    }
+}
