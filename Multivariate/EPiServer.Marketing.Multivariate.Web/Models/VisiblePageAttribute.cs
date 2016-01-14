7e5cf5de9b15f9e1b27eec040f26eab6e70a43bd
Jason Masterson <jason.masterson@episerver.com>
2016-01-14 16:28:11 -0500

    added visible page validation to the model

---
+++ Multivariate/EPiServer.Marketing.Multivariate.Web/Models/VisiblePageAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using EPiServer.ServiceLocation;
+using EPiServer.Core;
+
+namespace EPiServer.Marketing.Multivariate.Web.Models
+{
+    public class VisiblePageAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                var contentrepo = ServiceLocator.Current.GetInstance<IContentRepository>();
+
+                var itemValue = int.Parse(value.ToString());
+                // need to get guid for pages from the page picker content id's we get
+                var itemRef = contentrepo.Get<PageData>(new ContentReference(itemValue));
+
+                return PageDataExtensions.IsVisibleOnSite(itemRef);
+            }
+            return false;
+        }
+    }
+}
