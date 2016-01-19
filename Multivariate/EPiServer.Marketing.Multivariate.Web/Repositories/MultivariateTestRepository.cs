92cce854b1ac553d3bc64887bc78f6f350092f5b
adam-pitcher <apitcher@apitcher.net>
2016-01-19 15:01:59 -0500

    Changes (and related Unit Tests) to prevent a test from appending Origin and Variant content and related result sets to a test which was causing edited tests to grow beyond 2 entities.

--- Multivariate/EPiServer.Marketing.Multivariate.Web/Repositories/MultivariateTestRepository.cs
+++ Multivariate/EPiServer.Marketing.Multivariate.Web/Repositories/MultivariateTestRepository.cs
@@ -7,6 +7,7 @@ using EPiServer.ServiceLocation;
 using EPiServer.Marketing.Multivariate.Model;
 using EPiServer.Marketing.Multivariate.Web.Models;
 using System.Diagnostics.CodeAnalysis;
+using EPiServer.Marketing.Multivariate.Model.Enums;
 
 namespace EPiServer.Marketing.Multivariate.Web.Repositories
 {
@@ -35,41 +36,37 @@ namespace EPiServer.Marketing.Multivariate.Web.Repositories
 
 
         /// <summary>
-        /// Creates a test based on supplied multivariate test data
+        /// Creates or modifies a test based on supplied multivariate test data
         /// </summary>
         /// <param name="testData"></param>
         public Guid CreateTest(MultivariateTestViewModel testData)
         {
-            IContentRepository contentrepo = _serviceLocator.GetInstance<IContentRepository>();
-
-            // need to get guid for pages from the page picker content id's we get
-            var originalItemRef = contentrepo.Get<IContent>(new ContentReference(testData.OriginalItem));
-            var variantItemRef = contentrepo.Get<IContent>(new ContentReference(testData.VariantItem));
-
             var tm = _serviceLocator.GetInstance<IMultivariateTestManager>();
 
-            var test = new MultivariateTest
+            //attempt to get a saved test based on incomming id
+            MultivariateTest test = tm.Get(testData.id) as MultivariateTest;
+
+
+            if (test == null) //no test exists
             {
-                Id = testData.id,
-                Title = testData.Title,
-                Owner = Security.PrincipalInfo.CurrentPrincipal.Identity.Name,
-                OriginalItemId = originalItemRef.ContentGuid,
-                StartDate = testData.StartDate,
-                EndDate = testData.EndDate,
-                Variants = new List<Variant>()
-                {
-                    new Variant() {Id = Guid.NewGuid(), VariantId = variantItemRef.ContentGuid}
-                },
-                KeyPerformanceIndicators = new List<KeyPerformanceIndicator>()
-                {
-                    new KeyPerformanceIndicator() {Id = Guid.NewGuid(), KeyPerformanceIndicatorId = Guid.NewGuid()},
-                },
-                MultivariateTestResults = new List<MultivariateTestResult>()
-                {
-                    new MultivariateTestResult() {Id = Guid.NewGuid(), ItemId = originalItemRef.ContentGuid},
-                    new MultivariateTestResult() {Id = Guid.NewGuid(), ItemId = variantItemRef.ContentGuid}
-                }
-            };
+                //if there isn't a test in the system just convert.
+                test = ConvertToMultivariateTest(testData);
+            }
+            else if (test.TestState == TestState.Inactive)
+            {
+                //they could be changing any or all fields so easier
+                //to delete the existing test and let ef handle cleanup. 
+                //Then recreate test to avoid potenitally orphaning
+                //origin, variant and test result db entries
+                tm.Delete(testData.id);
+                test = ConvertToMultivariateTest(testData);
+            }
+            if (test.TestState == TestState.Active)
+            {
+                //Per current rules, Active tests can only modify the end date.
+                test.EndDate = testData.EndDate;
+
+            } 
 
             return tm.Save(test);
         }
@@ -137,6 +134,41 @@ namespace EPiServer.Marketing.Multivariate.Web.Repositories
             };
             
             return testModel;
+        }
+
+        public MultivariateTest ConvertToMultivariateTest(MultivariateTestViewModel testToConvert)
+        {
+            IContentRepository contentrepo = _serviceLocator.GetInstance<IContentRepository>();
+
+            // need to get guid for pages from the page picker content id's we get
+            var originalItemRef = contentrepo.Get<IContent>(new ContentReference(testToConvert.OriginalItem));
+            var variantItemRef = contentrepo.Get<IContent>(new ContentReference(testToConvert.VariantItem));
+
+            var test = new MultivariateTest
+            {
+                Id = testToConvert.id,
+                Title = testToConvert.Title,
+                Owner = Security.PrincipalInfo.CurrentPrincipal.Identity.Name,
+                OriginalItemId = originalItemRef.ContentGuid,
+                StartDate = testToConvert.StartDate,
+                EndDate = testToConvert.EndDate,
+                Variants = new List<Variant>()
+                {
+                    new Variant() {Id = Guid.NewGuid(), VariantId = variantItemRef.ContentGuid}
+                },
+                KeyPerformanceIndicators = new List<KeyPerformanceIndicator>()
+                {
+                    new KeyPerformanceIndicator() {Id = Guid.NewGuid(), KeyPerformanceIndicatorId = Guid.NewGuid()},
+                },
+                MultivariateTestResults = new List<MultivariateTestResult>()
+                {
+                    new MultivariateTestResult() {Id = Guid.NewGuid(), ItemId = originalItemRef.ContentGuid},
+                    new MultivariateTestResult() {Id = Guid.NewGuid(), ItemId = variantItemRef.ContentGuid}
+                }
+            };
+
+            return test;
+
         }
 
         public MultivariateTestResult GetWinningTestResult(MultivariateTestViewModel test)
