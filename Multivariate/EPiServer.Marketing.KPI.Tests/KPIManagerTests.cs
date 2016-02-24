ea517d4fce05886cf45ee3415a739c7482234ed3
Brian Petrowicz <brian.petrowicz@episerver.com>
2016-02-24 07:51:53 -0500

    MAR-64 - add unit tests for kpi object

--- Multivariate/EPiServer.Marketing.KPI.Tests/KPIManagerTests.cs
+++ Multivariate/EPiServer.Marketing.KPI.Tests/KPIManagerTests.cs
@@ -1,7 +1,11 @@
-﻿using EPiServer.Marketing.KPI.Manager;
+﻿using EPiServer.Marketing.KPI.Model;
+using EPiServer.Marketing.KPI.Manager;
 using EPiServer.Marketing.KPI.DataAccess;
 using Moq;
 using System;
+using System.Collections.Generic;
+using EPiServer.Marketing.KPI.Dal;
+using EPiServer.Marketing.KPI.Model.Enums;
 using Xunit;
 using EPiServer.ServiceLocation;
 
@@ -18,7 +22,6 @@ namespace EPiServer.Marketing.KPI.Test
             _kpiDataAccess = new Mock<IKpiDataAccess>();
 
             _serviceLocator.Setup(sl => sl.GetInstance<IKpiDataAccess>()).Returns(_kpiDataAccess.Object);
-
             return new KpiManager(_serviceLocator.Object);
         }
 
@@ -30,5 +33,35 @@ namespace EPiServer.Marketing.KPI.Test
             manager.Get(aGuid);
             _kpiDataAccess.Verify(kpiDa => kpiDa.Get(It.Is<Guid>(arg => arg.Equals(aGuid))), "Data access was not called with the expected GUID");
         }
+
+        [Fact]
+        public void KpiTestManager_CallsSaveWithKpi()
+        {
+            var kpi = new Kpi();
+            var tm = GetUnitUnderTest();
+            tm.Save(kpi);
+
+            _kpiDataAccess.Verify(da => da.Save(It.Is<Kpi>(arg => arg.Equals(kpi))),
+                "DataAcessLayer Save was never called or kpi did not match.");
+        }
+
+        [Fact]
+        public void KpiTestManager_CallsDeleteWithGuid()
+        {
+            var theGuid = new Guid("A2AF4481-89AB-4D0A-B042-050FECEA60A3");
+            var manager = GetUnitUnderTest();
+            manager.Delete(theGuid);
+
+            _kpiDataAccess.Verify(da => da.Delete(It.Is<Guid>(arg => arg.Equals(theGuid))),
+                "DataAcessLayer Delete was never called or Guid did not match.");
+        }
+
+        [Fact]
+        public void Kpi_Success_Throws_Exception()
+        {
+            var kpi = new Kpi();
+            Assert.Throws<NotImplementedException>(() => kpi.Success(new object()));
+        }
+
     }
 }
