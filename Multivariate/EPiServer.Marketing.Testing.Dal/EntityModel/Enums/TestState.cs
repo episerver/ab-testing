e1f3cb14261e79bccd4dbc8a77b8fafba056129f
Jason Masterson <jason.masterson@episerver.com>
2016-02-24 10:29:11 -0500

    Adding in files that got moved... odd.

---
+++ Multivariate/EPiServer.Marketing.Testing.Dal/EntityModel/Enums/TestState.cs
@@ -0,0 +1,10 @@
+﻿namespace EPiServer.Marketing.Testing.Dal.Entity.Enums
+{
+    public enum TestState
+    {
+        Inactive = 0,
+        Active = 1,
+        Done = 2,
+        Archived = 3
+    }
+}
