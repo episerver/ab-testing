f6a12658efab99faa4fbf5ae12168b1c311806ba
Saivenkat Thumala <Saivenkat.Thumala@ektron.com>
2016-03-02 15:09:47 -0500

    This file was missing in previous check-in somehow.

--- Multivariate/src/EPiServer.Marketing.Testing.TestPages/Controllers/ApiTestingController.cs
+++ Multivariate/src/EPiServer.Marketing.Testing.TestPages/Controllers/ApiTestingController.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-////using Microsoft.AspNet.Mvc; // use  when we target for netcore50
 using System.Web.Mvc;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
@@ -9,13 +8,19 @@ using EPiServer.Marketing.Testing.Data.Enums;
 using EPiServer.Marketing.Testing.TestPages.ApiTesting;
 using EPiServer.Marketing.Testing.TestPages.Models;
 using EPiServer.ServiceLocation;
+using System.Threading;
+using System.Diagnostics;
+using EPiServer.Marketing.Testing.Messaging;
 
 namespace EPiServer.Marketing.Testing.TestPages.Controllers
 {
+
     public class ApiTestingController : Controller
     {
+        public static IntegrationTestModel AutoCreateModel = new IntegrationTestModel()
+        { BaseTestName = "Automation_", Conversions = 1000, Views = 1000, NumberOfTests = 10, };
+        public static Stopwatch messageQueueWatch = new Stopwatch();
 
-        ////public IActionResult Index() // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore50
         public ActionResult Index()
         {
             ApiTestingRepository testLib = new ApiTestingRepository();
@@ -29,17 +34,161 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
 
         //Generates index with filters applied.
         [HttpPost]
-        ////public IActionResult FilteredTests(ViewModel viewModel) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult FilteredTests(ViewModel viewModel)
         {
             ApiTestingRepository testLib = new ApiTestingRepository();
-            //ViewModel vm = new ViewModel { Tests = testLib.GetTests(viewModel)};
             viewModel.Tests = testLib.GetTests(viewModel);
             return View("Index", viewModel);
         }
 
         [HttpGet]
-        ////public IActionResult CreateAbTest() // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
+        public ActionResult AutoCreateAbTest()
+        {
+            return View(AutoCreateModel);
+        }
+
+        static int TestID = 0;
+        [HttpPost]
+        public ActionResult AutoCreateAbTest(IntegrationTestModel model)
+        {
+            AutoCreateModel.Conversions = model.Conversions;
+            AutoCreateModel.Views = model.Views;
+            AutoCreateModel.BaseTestName = model.BaseTestName;
+            AutoCreateModel.NumberOfTests = model.NumberOfTests;
+            AutoCreateModel.ElapsedTimeToCreateTests = TimeSpan.Zero;
+            AutoCreateModel.ElapsedTimeToEmitMessages = TimeSpan.Zero;
+            AutoCreateModel.ElapsedTimeToProcessAllMessages = TimeSpan.Zero;
+
+            ITestManager testManager = ServiceLocator.Current.GetInstance<ITestManager>();
+            var tests = testManager.GetTestList(new TestCriteria());
+            foreach (var test in tests)
+            {
+                if (test.Title.Contains(model.BaseTestName))
+                {
+                    testManager.Delete(test.Id);
+                }
+            }
+
+            var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
+            var startpage = contentLoader.Get<PageData>(ContentReference.StartPage);
+            var rootpage = contentLoader.Get<PageData>(ContentReference.RootPage);
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            for (int zzz = 0; zzz < model.NumberOfTests; zzz++)
+            {
+                var testId = Guid.NewGuid();
+                ABTest test = new ABTest()
+                {
+                    Id = testId,
+                    CreatedDate = DateTime.Now,
+                    EndDate = DateTime.Now.AddDays(5),
+                    KeyPerformanceIndicators = new List<KeyPerformanceIndicator>(),
+                    LastModifiedBy = "Automation",
+                    ModifiedDate = DateTime.Now,
+                    OriginalItemId = startpage.ContentGuid,
+                    Owner = "Automation",
+                    StartDate = DateTime.Now.AddDays(1),
+                    State = TestState.Active,
+                    TestResults = new List<TestResult>(),
+                    Title = "Automation_" + TestID++,
+                    Variants = new List<Variant>() {
+                                    new Variant() { Id = Guid.NewGuid(), ItemId=Guid.NewGuid(), ItemVersion = 1},
+                                    new Variant() { Id = Guid.NewGuid(), ItemId=Guid.NewGuid(), ItemVersion = 2}
+                                }
+                };
+                test.TestResults.Add(new TestResult { Id = Guid.NewGuid(), ItemId = test.Variants[0].Id });
+                test.TestResults.Add(new TestResult { Id = Guid.NewGuid(), ItemId = test.Variants[1].Id });
+
+                testManager.Save(test);
+            }
+            AutoCreateModel.ElapsedTimeToCreateTests = watch.Elapsed;
+            messageQueueWatch.Restart();
+
+            UpdateCountsThread t = new UpdateCountsThread(model);
+            Thread oThread = new Thread(new ThreadStart(t.doit));
+            oThread.Start();
+            Thread.Sleep(500); // wait for the thread to start doing something...
+            return RedirectToAction("AutoCreateAbTest");
+        }
+
+        private class CheckQueueSizeThread
+        {
+            IntegrationTestModel _model;
+
+            public CheckQueueSizeThread(IntegrationTestModel model)
+            {
+                _model = model;
+            }
+
+            public void doit()
+            {
+                bool done = false;
+                do
+                {
+                    AutoCreateModel.ElapsedTimeToProcessAllMessages = messageQueueWatch.Elapsed;
+                    Thread.Sleep(500);
+
+                    IMessagingManager mm = ServiceLocator.Current.GetInstance<IMessagingManager>();
+                    AutoCreateModel.QueueCount = mm.Count;
+                    if (AutoCreateModel.QueueCount == 0)
+                    {
+                        messageQueueWatch.Start();
+                        done = true;
+                    }
+
+                } while (!done);
+            }
+        }
+
+        private class UpdateCountsThread
+        {
+            IntegrationTestModel _model;
+
+            public UpdateCountsThread(IntegrationTestModel model)
+            {
+                _model = model;
+
+            }
+
+            public void doit()
+            {
+                // now update the views and conversions
+                ITestManager testManager = ServiceLocator.Current.GetInstance<ITestManager>();
+                var tests = testManager.GetTestList(new TestCriteria());
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                messageQueueWatch.Start();
+
+                foreach (var test in tests)
+                {
+                    for (int x = 0; x < _model.Views; x++)
+                    {
+                        testManager.EmitUpdateCount(test.Id, test.Variants[0].Id, CountType.View);
+                        testManager.EmitUpdateCount(test.Id, test.Variants[1].Id, CountType.View);
+                    }
+                    for (int x = 0; x < _model.Conversions; x++)
+                    {
+                        testManager.EmitUpdateCount(test.Id, test.Variants[0].Id, CountType.Conversion);
+                        testManager.EmitUpdateCount(test.Id, test.Variants[1].Id, CountType.Conversion);
+                    }
+
+                    IMessagingManager mm = ServiceLocator.Current.GetInstance<IMessagingManager>();
+                    AutoCreateModel.QueueCount = mm.Count;
+                }
+
+                AutoCreateModel.ElapsedTimeToEmitMessages = watch.Elapsed;
+                AutoCreateModel.ElapsedTimeToProcessAllMessages = messageQueueWatch.Elapsed;
+
+                CheckQueueSizeThread t = new CheckQueueSizeThread(_model);
+                Thread oThread = new Thread(new ThreadStart(t.doit));
+                oThread.Start();
+                oThread.Join();
+            }
+        }
+
+        [HttpGet]
         public ActionResult CreateAbTest()
         {
             ABTest multiVariateTest = new ABTest();
@@ -54,7 +203,6 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
         }
 
         [HttpPost]
-        ////public IActionResult CreateAbTest(ABTest multivariateTestData) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult CreateABTest(ABTest multivariateTestData)
         {
             ApiTestingRepository testLib = new ApiTestingRepository();
@@ -65,12 +213,11 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
 
                 return View("TestDetails", mtm.Get(savedTestId));
             }
-
             return View(multivariateTestData);
+
         }
 
         [HttpGet]
-        ////public IActionResult CreateAbTest(ABTest multivariateTestData) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult UpdateAbTest(string id)
         {
             TestManager mtm = new TestManager();
@@ -80,7 +227,6 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
         }
 
         [HttpPost]
-        ////public IActionResult UpdateAbTest(ABTest dataToSave) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult UpdateAbTest(ABTest dataToSave)
         {
             ApiTestingRepository testLib = new ApiTestingRepository();
@@ -95,13 +241,14 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
             return View("CreateAbTest", dataToSave);
         }
 
-        ////public IActionResult GetAbTestById(string id) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult GetAbTestById(string id)
         {
             TestManager mtm = new TestManager();
             ABTest returnedTest = mtm.Get(Guid.Parse(id)) as ABTest;
 
             return View("TestDetails", returnedTest);
+
+
         }
 
         /// <summary>
@@ -113,8 +260,6 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
         /// Null or Empty is returned or error is thrown.
         /// </summary>
         /// <returns></returns>
-
-        ////public IActionResult GetAbTestList(string id) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult GetAbTestList(string id)
         {
             ApiTestingRepository testLib = new ApiTestingRepository();
@@ -123,13 +268,15 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
             return View(returnedTestList);
         }
 
-        ////public IActionResult TestDetails(ABTest testDetails) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
+
+
+
+
         public ActionResult TestDetails(ABTest testDetails)
         {
             return View(testDetails);
         }
 
-        ////public IActionResult DeleteAbTest(Guid id) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult DeleteAbTest(Guid id)
         {
             TestManager mtm = new TestManager();
@@ -139,7 +286,6 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
 
         }
 
-        ////public IActionResult RunAbTests(string id) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult RunAbTests(string id)
         {
             ApiTestingRepository testLib = new ApiTestingRepository();
@@ -148,7 +294,6 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
             return View("TestDetails", multiVariateTest);
         }
 
-        ////public IActionResult StartAbTest(string id) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult StartAbTest(string id)
         {
             ApiTestingRepository testLib = new ApiTestingRepository();
@@ -157,7 +302,6 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
             return View("TestDetails", multiVariateTest);
         }
 
-        ////public IActionResult StartTest(string id) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult StartTest(string id)
         {
             ApiTestingRepository testLib = new ApiTestingRepository();
@@ -166,7 +310,6 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
             return View("TestDetails", multiVariateTest);
         }
 
-        ////public IActionResult ArchiveAbTest(string id) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult ArchiveAbTest(string id)
         {
             TestManager mtm = new TestManager();
@@ -176,7 +319,6 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
             return View("TestDetails", multivariateTest);
         }
 
-        ////public IActionResult UpdateView(string id, string itemid) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult UpdateView(string id, string itemid)
         {
             TestManager mtm = new TestManager();
@@ -186,7 +328,6 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
             return View("TestDetails", multivariateTest);
         }
 
-        ////public IActionResult UpdateConversion(string id, string itemid) // IActionResult is in Microsoft.Aspnet.Mvc namespace which will be addressed when we target for netcore
         public ActionResult UpdateConversion(string id, string itemid)
         {
             TestManager mtm = new TestManager();
@@ -224,6 +365,20 @@ namespace EPiServer.Marketing.Testing.TestPages.Controllers
             public string Name { get; set; }
             public string Version { get; set; }
             public string Reference { get; set; }
+        }
+
+        public class IntegrationTestModel
+        {
+            public int NumberOfTests { get; set; }
+            public int Views { get; set; }
+            public int Conversions { get; set; }
+            public string BaseTestName { get; set; }
+
+            public TimeSpan ElapsedTimeToCreateTests { get; set; }
+            public TimeSpan ElapsedTimeToEmitMessages { get; set; }
+            public TimeSpan ElapsedTimeToProcessAllMessages { get; set; }
+
+            public int QueueCount { get; set; }
         }
     }
 }
