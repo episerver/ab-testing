529831c56fee19c085681d652b591c0c63a61cfb
Dmytro Duk <Dmytro.Duk@episerver.com>
2016-03-07 15:40:56 +0100

    Using the same version for messaging. MAR-180

--- Multivariate/EPiServer.Marketing.Messaging/EPIServer.Marketing.Messaging.Test/Properties/AssemblyInfo.cs
+++ Multivariate/EPiServer.Marketing.Messaging/EPIServer.Marketing.Messaging.Test/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("75d6e462-f8b3-49ab-8b69-753d4fa55229")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
