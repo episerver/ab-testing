f41ff99b3b791c9a42d3bdfe60ddba76393e1621
adam-pitcher <apitcher@apitcher.net>
2016-03-27 15:16:47 -0400

    Synching TestDataCookie due to bad push.

---
+++ src/EPiServer.Marketing.Testing.Core/DataClass/TestDataCookie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EPiServer.Marketing.Testing.Core.DataClass
+{
+    public class TestDataCookie
+    {
+        public bool TestParticipant { get; set; }
+        public Guid TestId { get; set; }
+        public Guid TestContentId { get; set; }
+        public Guid TestVariantId { get; set; }
+        public bool ShowVariant { get; set; }
+        public bool Viewed { get; set; }
+        public bool Converted { get; set; }
+    }
+}
