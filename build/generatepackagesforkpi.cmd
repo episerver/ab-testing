bdb850d7edd5ae1b8503b6fe1c73a6a55d808e19
brpe <brian.petrowicz@episerver.com>
2016-03-29 10:11:37 -0400

    update nuget package generation for kpi project

--- build/generatepackagesforkpi.cmd
+++ build/generatepackagesforkpi.cmd
@@ -1,17 +1,19 @@
-IF exist "..\KpiDeployment\lib" ( rd "..\KpiDeployment\lib" /s /q )
-
-md "..\KpiDeployment\lib"
-
 IF "%1"=="Debug" (set Configuration=Debug) ELSE (set Configuration=Release)
-xcopy "..\artifacts\"%Configuration%"\net451\EPiServer.Marketing.KPI.dll" "..\KpiDeployment\lib\"  /I /F /R /Y
 
-IF exist "..\KpiDeployment\*.nuspec" ( del "..\KpiDeployment\*.nuspec" /s /q )
-xcopy "..\src\EPiServer.Marketing.KPI\Package.nuspec" "..\KpiDeployment\"  /I /F /R /Y
+set PackagePath="..\artifacts\%Configuration%\KpiDeployment"
+set ProjectPath="..\src\EPiServer.Marketing.KPI"
 
-rem cd "..\KpiDeployment\" 
-rem rename EPiServer.Marketing.Messaging.nuspec Package.nuspec
+IF exist "%PackagePath%" ( rd "%PackagePath%" /s /q )
 
-"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" powershell -ExecutionPolicy ByPass -File "buildpackage.ps1" "..\KpiDeployment" "..\src\\"
+md "%PackagePath%\lib"
 
-xcopy "..\KpiDeployment"\*nupkg "..\Deployment"  /I /F /R /Y
+xcopy "..\artifacts\%Configuration%\net45\EPiServer.Marketing.KPI.dll" "%PackagePath%\lib\"  /I /F /R /Y
+
+xcopy "%ProjectPath%\Package.nuspec" "%PackagePath%\"  /I /F /R /Y
+
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" powershell -ExecutionPolicy ByPass -File "buildpackage.ps1" "%PackagePath%" ".."
+
+xcopy "%PackagePath%"\*nupkg "..\artifacts"  /I /F /R /Y
+
+rd "%PackagePath%" /s /q
 
