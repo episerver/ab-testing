274a6cc729d7f9ef5fbd822fbeebe56631b92eeb
Herb McCarthy <Herb.McCarthy@episerver.com>
2016-04-01 10:45:14 -0400

    Modifed landingpage kpi class and made a generic ContentComparatorKPI which is much simplier and more generic.

---
+++ src/EPiServer.Marketing.KPI/Common/ContentComparatorKPI.cs
@@ -0,0 +1,28 @@
+﻿using EPiServer.Core;
+using EPiServer.Marketing.KPI.Manager.DataClass;
+using System;
+
+namespace EPiServer.Marketing.KPI.Common
+{
+    /// Common KPI class that can be used to compare IContent Guid values 
+    /// 
+    public class ContentComparatorKPI : Kpi
+    {
+        private Guid _ContentGuid;
+
+        public ContentComparatorKPI(Guid contentGuid)
+        {
+            Properties = contentGuid.ToString();
+        }
+
+        new public string Properties {
+            set { _ContentGuid = Guid.Parse(value);  }
+            get { return _ContentGuid.ToString();  }
+        }
+
+        public Boolean Evaluate(IContent content)
+        {
+            return _ContentGuid.Equals(content.ContentGuid);
+        }
+    }
+}
