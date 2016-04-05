f10be96ffdba4939766f5cc0599f220e8f6cc6e8
Adam Pitcher <adam.pitcher@episerver.com>
2016-04-05 16:20:27 -0400

    json changes to require System.Runtime.Caching Testhandler unit test file.

---
+++ test/EPiServer.Marketing.Testing.Test/Web/TestHandlerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using EPiServer.Marketing.Testing.Web;
+using Moq;
+using Xunit;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace EPiServer.Marketing.Testing.Test.Web
+{
+    
+    public class TestHandlerTests
+    {
+        Mock<ITestHandler> _mockTestHandler = new Mock<ITestHandler>();
+
+
+    }
+
+
+}
