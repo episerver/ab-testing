14476589bc2ee5fa232652ff373523979785730a
Adam Pitcher <adam.pitcher@episerver.com>
2016-04-13 13:49:08 -0400

    Testing Context Helper

---
+++ src/EPiServer.Marketing.Testing.Web/Helpers/TestingContextHelper.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Web;
+
+namespace EPiServer.Marketing.Testing.Web.Helpers
+{
+    public class TestingContextHelper
+    {
+        public bool IsInSystemFolder()
+        {
+            return HttpContext.Current.Request.RawUrl.ToLower()
+                .Contains(EPiServer.Shell.Paths.ProtectedRootPath.ToLower());
+        }
+    }
+}
