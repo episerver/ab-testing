88d67343afc9ca024314786e5da22393685f1494
Herb McCarthy <Herb.McCarthy@episerver.com>
2016-04-29 09:45:55 -0400

    Initial checkin - job and unit test

--- src/EPiServer.Marketing.Testing.Web/Repositories/MarketingTestingWebRepository.cs
+++ src/EPiServer.Marketing.Testing.Web/Repositories/MarketingTestingWebRepository.cs
@@ -112,7 +112,7 @@ namespace EPiServer.Marketing.Testing.Web.Repositories
                 Owner = GetCurrentUser(),
                 Description = testData.TestDescription,
                 Title = testData.TestTitle,
-                StartDate = DateTime.Parse(testData.StartDate).ToLocalTime(),
+                StartDate = DateTime.Parse(testData.StartDate).ToUniversalTime(),
                 EndDate = CalculateEndDateFromDuration(testData.StartDate, testData.TestDuration),
                 ParticipationPercentage = testData.ParticipationPercent,
                 Variants = new List<Variant>
@@ -144,7 +144,7 @@ namespace EPiServer.Marketing.Testing.Web.Repositories
 
         private DateTime? CalculateEndDateFromDuration(string startDate, int testDuration)
         {
-            DateTime endDate = DateTime.Parse(startDate).ToLocalTime();
+            DateTime endDate = DateTime.Parse(startDate).ToUniversalTime();
             return endDate.AddDays(testDuration);
         }
 
