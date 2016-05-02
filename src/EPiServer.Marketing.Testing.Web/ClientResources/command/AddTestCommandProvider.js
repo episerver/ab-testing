f1ff6671ba682847f8eba31357669b8a83db3565
jmasterson <jmasterson@ABTESTING>
2016-05-02 15:41:02 -0400

    MAR-204: fixed dependency in command provider

--- src/EPiServer.Marketing.Testing.Web/ClientResources/command/AddTestCommandProvider.js
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/command/AddTestCommandProvider.js
@@ -4,8 +4,9 @@
     // Parent class
     'epi-cms/component/command/_GlobalToolbarCommandProvider',
     // Other classes
-    'marketing-testing/command/AddTest'
-], function (declare, _GlobalToolbarCommandProvider, AddTest) {
+    'marketing-testing/command/AddTest',
+    'marketing-testing/command/CancelTest'
+], function (declare, _GlobalToolbarCommandProvider, AddTest, CancelTest) {
 
     return declare([_GlobalToolbarCommandProvider], {
 
