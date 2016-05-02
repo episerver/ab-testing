d9b33a220f7521f4dc5e7f1aa5df8b710044f799
Adam Pitcher <adam.pitcher@episerver.com>
2016-05-02 12:46:07 -0400

    Fix for  MAR 259:      Check if HttpContext is null to prevent attempting to resolve content from URL when context is not available.      Only attempt to get current page information if swapDisabled is false.
    Added unit test.

---
+++ test/EPiServer.Marketing.Testing.Test/Web/TestingContextHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using EPiServer.Marketing.Testing.Web.Helpers;
+using Moq;
+using Xunit;
+
+namespace EPiServer.Marketing.Testing.Test.Web
+{
+    public class TestingContextHelperTests : IDisposable
+    {
+        private TestingContextHelper _testingContextHelper ;
+
+        private TestingContextHelper GetUnitUnderTest(HttpContext context)
+        {
+            return new TestingContextHelper(context);
+        }
+
+        [Fact]
+        public void IsInSystemFolder_returns_true_if_context_is_null()
+        {
+            HttpContext context = null;
+            _testingContextHelper = GetUnitUnderTest(context);
+            var swapDisabled = _testingContextHelper.IsInSystemFolder();
+
+            Assert.True(swapDisabled);
+        }
+
+       
+        public void Dispose()
+        {
+            HttpContext.Current = null;
+        }
+    }
+}
