eaa5d0742457562922df28de3de686efe9c8a6d3
Adam Pitcher <Adam.Pitcher@episerver.com>
2016-05-05 15:35:18 -0400

    Context resolver now takes formatted string to act on testid or content id Includes updated unit tests for new code.

--- src/EPiServer.Marketing.Testing.Web/Context/MarketingTestingContextResolver.cs
+++ src/EPiServer.Marketing.Testing.Web/Context/MarketingTestingContextResolver.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using EPiServer.Marketing.Testing.Data;
-using EPiServer.Marketing.Testing.Data.Enums;
 using EPiServer.ServiceLocation;
 using EPiServer.Shell.Rest;
 
@@ -13,11 +13,13 @@ namespace EPiServer.Marketing.Testing.Web.Context
         public static readonly string UriPrefix = "epi.marketing.testing";
         private readonly ITestManager _testManager;
 
+        [ExcludeFromCodeCoverage]
         public MarketingTestingContextResolver(ITestManager testManager)
         {
             _testManager = testManager;
         }
 
+        [ExcludeFromCodeCoverage]
         public string Name
         {
             get { return UriPrefix; }
@@ -25,18 +27,45 @@ namespace EPiServer.Marketing.Testing.Web.Context
 
         public bool TryResolveUri(Uri uri, out ClientContextBase instance)
         {
-            Guid id = new Guid();
+            Guid id;
+            IMarketingTest marketingTest = new ABTest();
+
             instance = null;
 
-            if (uri.Segments.Length <= 1 || !Guid.TryParse(uri.Segments[1], out id))
+            //check url to resolve for actionable keys
+            if (uri.Segments.Length <= 1 || (!uri.Segments[1].Contains("testid") &&
+                !uri.Segments[1].Contains("contentid")))
             {
                 return false;
             }
 
-           var marketingTest =
-                _testManager
-                    .GetTestByItemId(id)
-                    .FirstOrDefault();
+            //check url for properly formatted property string
+            if (uri.Segments[1].IndexOf('=') == -1)
+            {
+                return false;
+
+            }
+
+            //check url for proper guid
+            if (!Guid.TryParse(uri.Segments[1].Split('=')[1], out id))
+            {
+                return false;
+            }
+
+            //if url is properly formatted with the correct data
+            //get back the test based on the guid type given
+            var idType = uri.Segments[1].Split('=')[0];
+            id = Guid.Parse(uri.Segments[1].Split('=')[1]);
+
+            switch (idType)
+            {
+                case "testid":
+                    marketingTest = _testManager.Get(id);
+                    break;
+                case "contentid":
+                    marketingTest = _testManager.GetTestByItemId(id).FirstOrDefault();
+                    break;
+            }
 
             if (marketingTest == null)
             {
@@ -50,6 +79,7 @@ namespace EPiServer.Marketing.Testing.Web.Context
                 VersionAgnosticUri = uri,
                 Name = marketingTest.Title,
                 DataType = typeof(IMarketingTest).FullName.ToLowerInvariant(),
+                Data = marketingTest,
                 CustomViewType = "marketing-testing/views/MultivariateTestDetailsView"
             };
 
