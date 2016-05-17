f86c66392a6a698eb69f7af32812ecdeb9642a9e
Adam Pitcher <adam.pitcher@episerver.com>
2016-05-17 13:45:14 -0400

    Filled in details of pick winner view configuration

--- src/EPiServer.Marketing.Testing.Web/Initializers/MarketingTestPickWinnerViewConfiguration.cs
+++ src/EPiServer.Marketing.Testing.Web/Initializers/MarketingTestPickWinnerViewConfiguration.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Mvc;
+using EPiServer.Marketing.Testing.Data;
+using EPiServer.ServiceLocation;
+using EPiServer.Shell;
 
 namespace EPiServer.Marketing.Testing.Web.Initializers
 {
-    public class MarketingTestPickWinnerViewConfiguration
+    [ServiceConfiguration(typeof(ViewConfiguration))]
+    [ExcludeFromCodeCoverage]
+    public class MarketingTestPickWinnerViewConfiguration : ViewConfiguration<IMarketingTest>
     {
+
+        public MarketingTestPickWinnerViewConfiguration()
+        {
+            Key = "MarketingTestPickWinnerView";
+            ControllerType = "marketing-testing/views/MarketingTestPickWinnerView";
+            ViewType = "marketing-testing/views/MarketingTestPickWinnerView";
+            HideFromViewMenu = true;
+
+        }
     }
 }
