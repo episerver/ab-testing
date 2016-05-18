560ae6cc8ed4e881afead28345088ce5b3b13e0e
Adam Pitcher <adam.pitcher@episerver.com>
2016-05-18 16:35:28 -0400

    Added remainder of missing fields in VIew. Fixed issues and added code to generate proper values, localized strings and formatting in javascript.

--- src/EPiServer.Marketing.Testing.Web/ClientResources/views/MarketingTestDetailsView.js
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/views/MarketingTestDetailsView.js
@@ -1,10 +1,13 @@
 ﻿define([
     "dojo/_base/declare",
     'epi/dependency',
+    'dojo/dom',
+    'dojo/dom-style',
+    "dojo/topic",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
-    "dojo/text!marketing-testing/views/MarketingTestDetailsView.html",
     "dijit/_WidgetsInTemplateMixin",
+    "dojo/text!marketing-testing/views/MarketingTestDetailsView.html",
     'dojo/i18n!marketing-testing/nls/MarketingTestingLabels',
     "epi/datetime",
     'xstyle/css!marketing-testing/css/style.css',
@@ -16,19 +19,21 @@
 ], function (
     declare,
     dependency,
+    dom,
+    domStyle,
+    topic,
     widgetBase,
     templatedMixin,
+    widgetsInTemplateMixin,
     template,
     resources,
-    widgetsInTemplateMixin,
     datetime
 
 ) {
-    return declare([widgetBase, templatedMixin, widgetsInTemplateMixin], {
+    return declare([widgetBase, templatedMixin, widgetsInTemplateMixin],
+    {
         templateString: template,
-
         resources: resources,
-
 
         constructor: function () {
             var contextService = epi.dependency.resolve("epi.shell.ContextService"), me = this;
@@ -36,27 +41,53 @@
         },
 
         postCreate: function () {
+            var publishedVariant, draftVariant;
             this.DetailsHeader.textContent = this.context.data.test.title;
 
             if (this.context.data.daysElapsed.indexOf("Test") !== -1) {
-                this.StartedText.textContent = this.context.data.daysElapsed;
+                this.daysElapsed.textContent = this.context.data.daysElapsed;
                 this.timeRemaining.textContent = this.context.data.daysRemaining;
             } else {
-                this.StartedText.textContent = datetime.toUserFriendlyString(this.context.data.test.StartDate);
-                this.Owner.textContent = this.context.data.test.Owner;
-                this.timeRemaining.textContent = this.context.data.daysRemaining + "days";
+                this.daysElapsed.textContent = resources.detailsview.test_started_label + ' ' + datetime.toUserFriendlyString(this.context.data.test.startDate) + ' ' + resources.detailsview.by;
+                this.Owner.textContent = this.context.data.test.owner;
+                this.timeRemaining.textContent = this.context.data.daysRemaining;
             }
 
             this.VersionAText.textContent = this.context.data.publishedVersionName;
             this.VersionAContentLink.textContent = this.context.data.publishedVersionContentLink;
+            this.PublishedUser.textContent = this.context.data.publishedVersionPublishedBy;
+            this.PublishedDate.textContent = datetime.toUserFriendlyString(this.context.data.publishedVersionPublishedDate);
 
             this.VersionBText.textContent = this.context.data.draftVersionName;
             this.VersionBContentLink.textContent = this.context.data.draftVersionContentLink;
             this.DraftUser.textContent = this.context.data.draftVersionChangedBy;
             this.DraftSavedDate.textContent = datetime.toUserFriendlyString(this.context.data.draftVersionChangedDate);
+
+
+            if (this.context.data.test.variants[0].itemversion === this.context.data.publishedVersionContentLink) {
+                publishedVariant = this.context.data.test.variants[0];
+                draftVariant = this.context.data.test.variants[1];
+            } else {
+                publishedVariant = this.context.data.test.variants[1];
+                draftVariant = this.context.data.test.variants[0];
+            }
+
+            this.firstVariantConversions.textContent = publishedVariant.conversions;
+            this.firstVariantViews.textContent = publishedVariant.views;
+            this.firstVariantPercentage.textContent = getPercent(publishedVariant.conversions, publishedVariant.views) + "%";
+            this.firstVariantPercentMeter.style.height = getPercent(publishedVariant.conversions, publishedVariant.views) * 1.5 + "px";
+
+            this.secondtVariantConversions.textContent = draftVariant.conversions;
+            this.secondVariantViews.textContent = draftVariant.views;
+            this.secondVariantPercentage.textContent = getPercent(draftVariant.conversions, draftVariant.views) + "%";
+            this.secondVariantPercentMeter.style.height = getPercent(draftVariant.conversions, draftVariant.views) * 1.5 + "px";
+
+            this.TestDescription.textContent = this.context.data.test.description;
+
             this.visitorPercentage.textContent = this.context.data.visitorPercentage;
             this.totalParticipants.textContent = this.context.data.totalParticipantCount;
-            this.TestDescription.textContent = this.context.data.test.Description;
+            //   this.firstVariantPercentMeter.height = "height:" + 300/getPercent(10, 100) + " px";
+
         },
 
         _onWinnerOptionClicked: function () {
@@ -76,5 +107,16 @@
         }
 
 
+
     });
+
+    function getPercent(visitors, conversions) {
+        if (conversions === 0) {
+            return 0;
+        }
+        var percent = (visitors / conversions) * 100;
+        return Math.round(percent);
+
+
+    }
 });
