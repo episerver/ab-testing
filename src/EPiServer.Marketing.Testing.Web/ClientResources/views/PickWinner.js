857e17357853831138065c95a46f906fece90616
Adam Pitcher <Adam.Pitcher@episerver.com>
2016-05-30 21:44:21 -0400

    Continued building out of pick a  winner view and backing javascript.

--- src/EPiServer.Marketing.Testing.Web/ClientResources/views/PickWinner.js
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/views/PickWinner.js
@@ -1,16 +1,42 @@
 ﻿define([
  'dojo/_base/declare',
+     'epi/dependency',
+
     'dijit/_WidgetBase',
     'dijit/_TemplatedMixin',
-    'dojo/text!marketing-testing/views/PickWinner.html'
+    'dojo/text!marketing-testing/views/PickWinner.html',
+    'dojo/i18n!marketing-testing/nls/MarketingTestingLabels'
+
 
 ], function (
     declare,
+    dependency,
     _WidgetBase,
     _TemplatedMixin,
     template
 ) {
     return declare([_WidgetBase, _TemplatedMixin], {
         templateString: template,
+        resources: resources,
+        contextHistory: null,
+
+
+        constructor: function () {
+            var contextService = dependency.resolve("epi.shell.ContextService"), me = this;
+            me.context = contextService.currentContext;
+
+        },
+
+        postcreate: function() {
+            this.contextHistory = dependency.resolve("epi.cms.BackContextHistory");
+
+        },
+
+        _onCloseButtonClick: function () {
+            this.contextHistory.closeAndNavigateBack(this);
+        }
+
+
+
     });
 });
