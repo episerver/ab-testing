2058f9c797a7e01166dcdec5a507148dfd67c6a4
Jason Masterson <jason.masterson@episerver.com>
2016-06-09 14:54:08 -0400

    MAR-313: metadataextender setup

---
+++ src/EPiServer.Marketing.Testing.Web/MetadataExtender/MarketingTestMetadataExtender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using EPiServer.Shell.ObjectEditing;
+using EPiServer.Cms.Shell.UI.ObjectEditing;
+using EPiServer.Cms.Shell.Extensions;
+
+namespace EPiServer.Marketing.Testing.Web.MetadataExtender
+{
+    /// <summary>
+    /// Sets Episerver content properties to be read only when that content is currently in a pending, active, or completed test
+    /// </summary>
+    public class MarketingTestMetadataExtender : IMetadataExtender
+    {
+        public void ModifyMetadata(ExtendedMetadata metadata, IEnumerable<Attribute> attributes)
+        {
+            var aContentMetadata = metadata as ContentDataMetadata;
+
+            if (aContentMetadata == null)
+                return;
+
+            var content = aContentMetadata.FindOwnerContent();
+        }
+    }
+}
