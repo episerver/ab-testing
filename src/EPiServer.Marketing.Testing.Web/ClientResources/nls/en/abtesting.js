fc73c1e1213705646b5d2c0885dfd018a5400154
Jason Masterson <jason.masterson@episerver.com>
2016-06-22 14:04:45 -0400

    Temp solution for getting the unit tests to work. Need to get the epi nls resources to work in the client test environment.

---
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/nls/en/abtesting.js
@@ -0,0 +1,20 @@
+﻿define({
+    //Mock labels for unit tests
+    addtestcommand: {
+        label_text: 'add label',
+        tooltip_text: 'add tooltip'
+    },
+    canceltestcommand: {
+        label_text: 'cancel label',
+        tooltip_text: 'cancel tooltip'
+    },
+    notificationbar: {
+        ongoing_test: 'ongoing',
+        scheduled_test: 'scheduled',
+        completed_test: 'complete',
+        details_link_text: 'detail link',
+        details_link_tooltip: 'detail tooltip',
+        winner_link_text: 'winner link',
+        winner_link_tooltip: 'winner tooltip'
+    }
+});
