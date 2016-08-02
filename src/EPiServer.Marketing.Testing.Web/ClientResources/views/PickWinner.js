6a49d05b502d8e4040f0b3628db576ba0c0580da
Adam Pitcher <adam.pitcher@episerver.com>
2016-08-02 16:27:06 -0400

    First pass at modifying pickwinner view.

--- src/EPiServer.Marketing.Testing.Web/ClientResources/views/PickWinner.js
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/views/PickWinner.js
@@ -1,29 +1,47 @@
 ﻿define([
-'dojo/_base/declare',
- 'epi/dependency',
- 'dojo/topic',
- 'dijit/_WidgetBase',
- 'dijit/_TemplatedMixin',
- 'dijit/_WidgetsInTemplateMixin',
- 'dojo/text!marketing-testing/views/PickWinner.html',
- 'epi/i18n!marketing-testing/nls/abtesting',
- 'epi/datetime',
- 'xstyle/css!marketing-testing/css/style.css',
- 'xstyle/css!marketing-testing/css/GridForm.css',
- 'dijit/form/Button'
+ "dojo/_base/declare",
+ "epi/dependency",
+ "dojo/dom",
+ "dijit/registry",
+ "dojo/dom-style",
+ "dojo/topic",
+ "dijit/_WidgetBase",
+ "dijit/_TemplatedMixin",
+ "dijit/_WidgetsInTemplateMixin",
+ "dojo/text!marketing-testing/views/PickWinner.html",
+ "epi/i18n!marketing-testing/nls/abtesting",
+ "epi/datetime",
+ "epi/username",
+ "dojo/dom-class",
+ "dojox/charting/Chart",
+ "dojox/charting/plot2d/Pie",
+ "xstyle/css!marketing-testing/css/ABTesting.css",
+ "xstyle/css!marketing-testing/css/GridForm.css",
+ "xstyle/css!marketing-testing/css/dijit.css",
+ "dijit/form/DropDownButton",
+ "dijit/TooltipDialog",
+ "dijit/form/Button"
+
 ], function (
     declare,
     dependency,
+    dom,
+    registry,
+    domStyle,
     topic,
-    _WidgetBase,
-    _TemplatedMixin,
+    widgetBase,
+    templatedMixin,
     widgetsInTemplateMixin,
     template,
     resources,
-    datetime
+    datetime,
+    username,
+    domClass,
+    chart,
+    pie
 
 ) {
-    return declare([_WidgetBase, _TemplatedMixin, widgetsInTemplateMixin],
+    return declare([widgetBase, templatedMixin, widgetsInTemplateMixin],
     {
         templateString: template,
         resources: resources,
@@ -37,39 +55,153 @@
 
         postCreate: function () {
             this._renderData();
+            this._renderChartData();
+
+        },
+
+        startup: function () {
         },
 
         _contextChanged: function (newContext) {
             var me = this;
-            if (!newContext || newContext.type != 'epi.marketing.testing') {
+            if (!newContext || newContext.type !== 'epi.marketing.testing') {
                 return;
             }
             me.context = newContext;
             me._renderData();
+            me._renderChartData();
         },
 
-        _onCloseButtonClick: function () {
+        _onCancelClick: function () {
             var me = this;
             me.contextParameters = { uri: "epi.cms.contentdata:///" + this.context.data.publishedVersionContentLink.split('_')[0] };
             topic.publish("/epi/shell/context/request", me.contextParameters);
         },
 
+        _renderData: function () {
+            var publishedVariant, draftVariant;
+
+            //Header and Test Start Information
+            this.pageName.textContent = this.context.data.test.title;
+
+            if (this.context.data.test.state === 0) {
+                this.headerStateAndElapsedText.textContent = resources.detailsview.test_status_not_started;
+                this.headerStartedText.textContent = resources.detailsview.test_scheduled +
+                    datetime.toUserFriendlyString(this.context.data.test.startDate);
+
+            } else if (this.context.data.test.state === 1) {
+                this.headerStateAndElapsedText
+                    .textContent = resources.detailsview.test_status_running + this.context.data.daysRemaining + " " + resources.detailsview.days_remaining;
+                this.headerStartedText.textContent = resources.detailsview.started +
+                    datetime.toUserFriendlyString(this.context.data.test.startDate) +
+                    " " +
+                    resources.detailsview.by +
+                    " " +
+                    username.toUserFriendlyString(this.context.data.test.owner);
+
+            } else if (this.contexxt.data.test.state === 3) {
+                this.headerStateAndElapsedText.textContent = resources.detailsview.test_status_completed;
+                this.headerStartedText.textContent = "";
+            }
+
+            this.testDuration.textContent = this.context.data.daysElapsed;
+            this.timeRemaining.textContent = this.context.data.daysRemaining;
+            this.selectedConfidenceLevel.textContent = this.context.data.test.confidenceLevel + "%";
+
+            //Published version data
+            this.publishedBy.textContent = username.toUserFriendlyString(this.context.data.publishedVersionPublishedBy);
+            this.datePublished.textContent = datetime.toUserFriendlyString(this.context.data.publishedVersionPublishedDate);
+
+            //Draft version data
+            this.savedBy.textContent = username.toUserFriendlyString(this.context.data.draftVersionChangedBy);
+            this.dateSaved.textContent = datetime.toUserFriendlyString(this.context.data.draftVersionChangedDate);
+
+            //Set the correct corresponding variant data
+            if (this.context.data.test.variants[0].itemVersion === this.context.data.publishedVersionContentLink.split('_')[0]) {
+                publishedVariant = this.context.data.test.variants[0];
+                draftVariant = this.context.data.test.variants[1];
+            } else {
+                publishedVariant = this.context.data.test.variants[1];
+                draftVariant = this.context.data.test.variants[0];
+            }
+
+            var publishedPercent = getPercent(publishedVariant.conversions, publishedVariant.views);
+            var variantPercent = getPercent(draftVariant.conversions, draftVariant.views);
+
+            //Published version views/conversions and meter
+            this.publishedVersionConversions.textContent = publishedVariant.conversions;
+            this.publishedVersionViews.textContent = publishedVariant.views;
+            this.publishedVersionPercentage.textContent = publishedPercent + "%";
+
+
+            //Draft version views/conversions and meter
+            this.variantConversions.textContent = draftVariant.conversions;
+            this.variantViews.textContent = draftVariant.views;
+            this.variantPercentage.textContent = variantPercent + "%";
+
+            //Test description, visitor percentage and total participants
+            this.testDescription.textContent = "\"" +
+                this.context.data.test.description +
+                "\" - " + username.toUserFriendlyString(this.context.data.test.owner);
+
+            this.visitorPercentageText.textContent = this.context.data.visitorPercentage;
+            this.totalParticipantsText.textContent = this.context.data.totalParticipantCount;
+
+            this.contentLinkAnchor.href = this.context.data.conversionLink;
+            this.contentLinkAnchor.textContent = this.context.data.conversionContentName;
+
+            //Set Test Result Status Icons and styles
+            //Icons are to show on content which is ahead in conversoins
+            //Styles change size and color of container to accomodate for border
+            //keeping things aligned properly.
+            //Styles and icons adjust based on A > B, A < B, A = B, and 0 and 
+            //whether test is active or complete.
+            var statusIndicatorClass = "noIndicator";
+
+            if (this.context.data.test.state === 1) {
+                statusIndicatorClass = "leadingContent";
+            }
+            else if (this.context.data.test.state > 2) {
+                statusIndicatorClass = "winningContent";
+            }
+
+            if (publishedPercent > variantPercent) {
+                domClass.replace(this.publishedStatusIcon, statusIndicatorClass);
+                domClass.replace(this.variantStatusIcon, "noIndicator");
+                domClass.replace(this.controlWrapper, "cardWrapper 2column controlLeaderBody");
+                domClass.replace(this.challengerWrapper, "cardWrapper 2column challengerDefaultBody");
+            }
+            else if (publishedPercent < variantPercent) {
+                domClass.replace(this.publishedStatusIcon, "noIndicator");
+                domClass.replace(this.variantStatusIcon, statusIndicatorClass);
+                domClass.replace(this.controlWrapper, "cardWrapper 2column controlTrailingBody");
+                domClass.replace(this.challengerWrapper, "cardWrapper 2column challengerLeaderBody");
+            }
+            else {
+                domClass.replace(this.publishedStatusIcon, "noIndicator");
+                domClass.replace(this.variantStatusIcon, "noIndicator");
+                domClass.replace(this.controlWrapper, "cardWrapper 2column controlDefaultBody");
+                domClass.replace(this.challengerWrapper, "cardWrapper 2column challengerDefaultBody");
+
+            }
+        },
+
         _onPublishedVersionClick: function () {
             this.store.put({
-                    publishedContentLink: this.context.data.publishedVersionContentLink,
-                    draftContentLink: this.context.data.draftVersionContentLink,
-                    winningContentLink: this.context.data.publishedVersionContentLink,
-                    testId: this.context.data.test.id
+                publishedContentLink: this.context.data.publishedVersionContentLink,
+                draftContentLink: this.context.data.draftVersionContentLink,
+                winningContentLink: this.context.data.publishedVersionContentLink,
+                testId: this.context.data.test.id
             }, { id: this.context.data.test.id }, { "options.incremental": false })  // Force a put
                     .then(function (data) {
                         var contextParameters = { uri: "epi.cms.contentdata:///" + data };
                         topic.publish("/epi/shell/context/request", contextParameters);
-                }).otherwise(function () {
-                    alert("Error Processing Winner: Unable to process and save selected version");
-                    console.log("Error occurred while processing winning content");
-            });
+                    }).otherwise(function () {
+                        alert("Error Processing Winner: Unable to process and save selected version");
+                        console.log("Error occurred while processing winning content");
+                    });
         },
-        
+
         _onVariantVersionClick: function () {
             this.store.put({
                 publishedContentLink: this.context.data.publishedVersionContentLink,
@@ -83,113 +215,42 @@
                 }).otherwise(function () {
                     alert("Error Processing Winner: Unable to process and save selected version");
                     console.log("Error occurred while processing winning content");
+                });
+        },
+
+        _renderChartData() {
+            dom.byId("controlPieChart").innerHTML = "";
+            dom.byId("challengerPieChart").innerHTML = "";
+            var controlPercentage = this.publishedVersionPercentage.textContent
+                .substr(0, this.publishedVersionPercentage.textContent.length - 1);
+            var challengerPercentage = this.variantPercentage.textContent
+                .substr(0, this.variantPercentage.textContent.length - 1);
+            this._displayPieChart("controlPieChart", Number(controlPercentage));
+            this._displayPieChart("challengerPieChart", Number(challengerPercentage));
+        },
+
+        _displayPieChart(node, data) {
+            var chartNode = dom.byId(node);
+            var pieChart = new chart(chartNode);
+
+            var chartData = [{
+                x: 1,
+                y: 100 - data,
+                fill: "#edebe9"
+            }, {
+                x: 1,
+                y: data,
+                fill: "#86c740"
+            }];
+
+            pieChart.addPlot("default", {
+                type: "Pie",
+                labels: false,
+                radius: 50
             });
-        },
-
-        _renderData: function () {
-            var publishedVariant, draftVariant, publishedConversionPercent, variantConversionPercent;
-
-            //reset the div sizes for the versions to prevent errors viewing multiple tests
-            this._resetVersionDivs();
-
-            this.store = dependency.resolve("epi.storeregistry").get("marketing.abtesting");
-            this.topic = this.topic || topic;
-
-            if (this.context.data.test.state === 0) {
-                this.scheduleStatusText.innerHTML =
-                    resources.pickwinnerview.test_not_started + "&nbsp" +
-                    resources.pickwinnerview.test_scheduled +
-                    datetime.toUserFriendlyString(this.context.data.test.startDate) + ".";
-            } else if (this.context.data.test.state === 1) {
-                this.scheduleStatusText.innerHTML =
-                    resources.pickwinnerview.started_by_text +
-                    "<span class='epi-username'>" + this.context.data.test.owner + "</span>" + ".&nbsp" +
-                    resources.pickwinnerview.test_scheduled_finish +
-                    datetime.toUserFriendlyString(this.context.data.test.endDate) + ".";
-            } else {
-                this.scheduleStatusText.innerHTML =
-                    resources.pickwinnerview.started_by_text +
-                    "<span class='epi-username'>" + this.context.data.test.owner + "</span>" + ".&nbsp" +
-                    resources.pickwinnerview.completed_text +
-                    datetime.toUserFriendlyString(this.context.data.test.endDate) + ".";
-            }
-
-            //display result significance
-            if (this.context.data.test.state === 2) {
-                if (this.context.data.test.isSignificant) {
-                    this.isSignificant.innerHTML = resources.pickwinnerview.result_is_significant;
-                } else {
-                    this.isSignificant.innerHTML = resources.pickwinnerview.result_is_not_significant;
-                }
-            }
-
-            //Set the correct corresponding variant data
-            if (this.context.data.test.variants[0].itemVersion == this.context.data.publishedVersionContentLink.split('_')[0]) {
-                publishedVariant = this.context.data.test.variants[0];
-                draftVariant = this.context.data.test.variants[1];
-            } else {
-                publishedVariant = this.context.data.test.variants[1];
-                draftVariant = this.context.data.test.variants[0];
-            }
-
-            publishedConversionPercent = getPercent(publishedVariant.conversions, publishedVariant.views);
-            variantConversionPercent = getPercent(draftVariant.conversions, draftVariant.views);
-
-            this._setVersionDivs(publishedConversionPercent, variantConversionPercent);
-
-            this.testTitle.textContent = this.context.data.test.title;
-
-            //Published version data
-            this.publishedVersionName.textContent = this.context.data.publishedVersionName;
-            this.publishedVersionContentLink.textContent = this.context.data.publishedVersionContentLink;
-            this.publishedByUser.textContent = this.context.data.publishedVersionPublishedBy;
-            this.publishedDate.textContent = datetime.toUserFriendlyString(this.context.data.publishedVersionPublishedDate);
-            this.publishedConversionPercent.textContent = publishedConversionPercent + "%";
-            //Draft version data
-            this.variantName.textContent = this.context.data.draftVersionName;
-            this.variantContentLink.textContent = this.context.data.draftVersionContentLink;
-            this.editedByUser.textContent = this.context.data.draftVersionChangedBy;
-            this.savedDate.textContent = datetime.toUserFriendlyString(this.context.data.draftVersionChangedDate);
-            this.variantConversionPercent.textContent = variantConversionPercent + "%";
-
-            //Test information and stats
-            this.descriptionText.textContent = this.context.data.test.description;
-            this.participationPercent.textContent = this.context.data.visitorPercentage + "%";
-            this.totalParticipants.textContent = this.context.data.totalParticipantCount;
-
-            if (this.context.data.test.state === 0) {
-                this.testDuration.textContent = this.context.data.daysElapsed;
-                this.durationText.textContent = "";
-            } else {
-                this.testDuration.textContent = this.context.data.daysElapsed;
-                this.durationText.textContent = resources.pickwinnerview.duration_text;
-            }
-
-            //Conversion content
-            this.contentLinkAnchor.href = this.context.data.conversionLink;
-            this.contentLinkAnchor.textContent = this.context.data.conversionContentName;
-        },
-
-        _resetVersionDivs: function () {
-            this.publishedDiv.style.height = "auto";
-            this.publishedDiv.style.width = "auto";
-            this.variantDiv.style.height = "auto";
-            this.variantDiv.style.width = "auto";
-        },
-
-        _setVersionDivs: function (publishedConversionPercent, variantConversionPercent) {
-            if (publishedConversionPercent == variantConversionPercent) {
-                //do not change the default div dimensions.
-            }
-            else if (publishedConversionPercent > variantConversionPercent) {
-                this.publishedDiv.style.height = "20em";
-                this.publishedDiv.style.width = "20em";
-            } else {
-                this.variantDiv.style.height = "20em";
-                this.variantDiv.style.width = "20em";
-            }
+            pieChart.addSeries("", chartData, { stroke: { width: 0 } });
+            pieChart.render();
         }
-
     });
 
     function getPercent(visitors, conversions) {
