0fa059c323abe3362e8db14871b064c4e9208c1b
Adam Pitcher <adam.pitcher@episerver.com>
2016-08-09 17:06:49 -0400

    * Removed duplicate strings section.  Test provides it's own mocked string to avoid issues if the nls/js file is no longer used.

--- src/EPiServer.Marketing.Testing.Web/ClientResources/nls/en/abtesting.js
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/nls/en/abtesting.js
@@ -17,18 +17,5 @@
         details_link_tooltip: 'detail tooltip',
         winner_link_text: 'winner link',
         winner_link_tooltip: 'winner tooltip'
-    },
-    mockTestTextHelper: {
-        test_status_running: "Test is running, ",
-        days_remaining: "day(s) remaining." ,
-        started: "started ",
-        test_status_completed: "Test completed, now go on and pick a winner...",
-        test_status_not_started: "Test has not yet started, ",
-        test_scheduled: "It is scheduled to begin ",
-        by: "by",
-        result_is_not_significant: "The results of this test are NOT significant.",
-        result_is_significant: "The results of this test are significant.",
-        early_pick_winner_message: "This test has not been completed, but you may pick a winner. Picking a winner now will end the test and publish the content chosen."
-    },
-  
+    }
 });
