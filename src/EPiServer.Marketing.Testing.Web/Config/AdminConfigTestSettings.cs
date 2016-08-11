0485fe9723fe883616527ac583625e1f19abd0c6
brpe <brian.petrowicz@episerver.com>
2016-08-11 14:11:02 -0400

    add new files and code cleanup

---
+++ src/EPiServer.Marketing.Testing.Web/Config/AdminConfigTestSettings.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace EPiServer.Marketing.Testing.Web.Config
+{
+    /// <summary>
+    /// Used to populate the dropdown in the admin config settings page for ABTesting
+    /// </summary>
+    public static class AvailableConfidenceLevels
+    {
+        public static IEnumerable<ConfidenceLevel> ConfidenceLevels = new List<ConfidenceLevel>
+        {
+            new ConfidenceLevel
+            {
+                Value = 99,
+                Name = "99%"
+            },
+            new ConfidenceLevel
+            {
+                Value = 98,
+                Name = "98%"
+            },
+            new ConfidenceLevel
+            {
+                Value = 95,
+                Name = "95%"
+            },
+            new ConfidenceLevel
+            {
+                Value = 90,
+                Name = "90%"
+            }
+        };
+    }
+
+    /// <summary>
+    /// ABTest settings that are configurable via the admin config page
+    /// </summary>
+    public class AdminConfigTestSettings
+    {
+        public int TestDuration { get; set; }
+
+        public int ParticipationPercentage { get; set; }
+
+        public int ConfidenceLevel { get; set; }
+    }
+
+    public class ConfidenceLevel
+    {
+        public string Name { get; set; }
+
+        public int Value { get; set; }
+    }
+
+}
---
+++ src/EPiServer.Marketing.Testing.Web/Controllers/ConfigController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Marketing.Testing.Web.Config;
+
+namespace EPiServer.Marketing.Testing.Web.Controllers
+{
+    [EPiServer.PlugIn.GuiPlugIn(Area = EPiServer.PlugIn.PlugInArea.AdminConfigMenu, Url = "/EPiServer/EPiserver.Marketing.Testing.Web/Config/Index", DisplayName = "AB Testing Configuration")]
+    public class ConfigController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new AdminConfigTestSettings() { TestDuration = 30, ConfidenceLevel = 98, ParticipationPercentage = 15 };
+
+            return View("~/modules/_protected/Episerver.Marketing.Testing/ClientResources/views/ConfigView.cshtml", model);
+        }
+    }
+
+    [InitializableModule]
+    public class CustomRouteInitialization : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            RouteTable.Routes.MapRoute(
+                null,
+                "EPiServer/EPiserver.Marketing.Testing.Web/Config/Index",
+                new { controller = "Config", action = "Index" },
+                new[] { "EPiServer.Marketing.Testing.Web.Controllers" });
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+    }
+}
+
