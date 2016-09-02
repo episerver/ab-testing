5bc7cf65d3ccc62a502bbaf4a292a958b789bf5d
Adam Pitcher <adam.pitcher@episerver.com>
2016-09-02 11:31:06 -0400

    archive, admin config merge

---
+++ src/EPiServer.Marketing.Testing.Web/Initializers/ArchiveViewConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using EPiServer.Marketing.Testing.Data;
+using EPiServer.ServiceLocation;
+using EPiServer.Shell;
+
+namespace EPiServer.Marketing.Testing.Web.Initializers
+{
+    [ServiceConfiguration(typeof(ViewConfiguration))]
+    [ExcludeFromCodeCoverage]
+    public class ArchiveViewConfiguration : ViewConfiguration<IMarketingTest>
+    {
+        public ArchiveViewConfiguration()
+        {
+            Key = "ArchiveView";
+            ControllerType = "marketing-testing/views/Archive";
+            ViewType = "marketing-testing/views/Archive";
+            HideFromViewMenu = true;
+        }
+    }
+}
