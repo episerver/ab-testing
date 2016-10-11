c60f27a9995baa44c1653a21baa2f2406ef0429a
Adam Pitcher <adam.pitcher@episerver.com>
2016-10-11 14:29:48 -0400

    * Modified kpi package generator to create kpi module resource (embedded lang file, module config and .zip)

--- build/generatepackagesforkpi.cmd
+++ build/generatepackagesforkpi.cmd
@@ -11,11 +11,26 @@ xcopy "..\artifacts\%Configuration%\net45\EPiServer.Marketing.KPI.dll" "%Package
 
 xcopy "%ProjectPath%\Package.nuspec" "%PackagePath%\"  /I /F /R /Y
 
-xcopy "..\src\Database"\KPI\*.sql "%PackagePath%\tools\epiupdates\sql"  /I /F /R 
+md "%PackagePath%\content\modules\_protected\EPiServer.Marketing.Kpi\EmbeddedLangFiles"
+xcopy "%ProjectPath%\EmbeddedLangFiles\EPiServer_Kpi_EN.xml" "%PackagePath%\content\modules\_protected\EPiServer.Marketing.Kpi\EmbeddedLangFiles\"  /I /F /R /Y
+xcopy "%ProjectPath%\module.config" "%PackagePath%\content\modules\_protected\EPiServer.Marketing.Kpi\"  /I /F /R /Y
+
+xcopy "..\src\Database"\KPI\*.sql "%PackagePath%\tools\epiupdates\sql"  /I /F /R
+
+md "%PackagePath%\temp"
+
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" powershell -ExecutionPolicy ByPass -File "buildzip.ps1" "%PackagePath%\content\modules\_protected\EPiServer.Marketing.Kpi" "%PackagePath%\temp\EPiServer.Marketing.Kpi.zip"
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" Unblock-File "%PackagePath%\temp\EPiServer.Marketing.Kpi.zip"
+
+for /d %%i in ("%PackagePath%\content\modules\_protected\EPiServer.Marketing.Kpi\*") do rd /s /q "%%i"
+del /q "%PackagePath%\content\modules\_protected\EPiServer.Marketing.Kpi\*.*"
+
+xcopy "%PackagePath%\temp\*.zip" "%PackagePath%\content\modules\_protected\EPiServer.Marketing.Kpi\"  /I /F /R /Y
+rd "%PackagePath%\temp" /s /q
+
 
 "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" powershell -ExecutionPolicy ByPass -File "buildpackage.ps1" "%PackagePath%" ".."
 
 xcopy "%PackagePath%"\*nupkg "..\artifacts"  /I /F /R /Y
 
 rd "%PackagePath%" /s /q
-
