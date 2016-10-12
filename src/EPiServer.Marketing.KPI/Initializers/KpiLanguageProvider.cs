c43d69f7bc4c44f8ad357f3fad3a4370209e5ee9
Adam Pitcher <adam.pitcher@episerver.com>
2016-10-12 13:21:28 -0400

    * Removed code to generate KPI zip and module folder

---
+++ src/EPiServer.Marketing.KPI/Initializers/KpiLanguageProvider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Framework.Localization;
+using EPiServer.Framework.Localization.XmlResources;
+using EPiServer.Marketing.KPI.Manager.DataClass;
+
+namespace EPiServer.Marketing.KPI.Initializers
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class KpiLanguageProvider : IInitializableModule
+    {
+        private const string ProviderName = "KpiCustomLanguageProvider";
+        public void Initialize(InitializationEngine context)
+        {
+            ProviderBasedLocalizationService localizationService = context.Locate.Advanced.GetInstance<LocalizationService>() as ProviderBasedLocalizationService;
+            if (localizationService != null)
+            {
+                EmbeddedXmlLocalizationProviderInitializer localizationProviderInitializer =
+                    new EmbeddedXmlLocalizationProviderInitializer();
+               
+                Assembly kpiAssembly = Assembly.GetAssembly(typeof(IKpi));
+
+                //a VPP with the path below must be registered in the sites configuration.
+                 XmlLocalizationProvider xmlProvider  = localizationProviderInitializer.GetInitializedProvider(ProviderName,kpiAssembly);
+                //Inserts the provider first in the provider list so that it is prioritized over default providers.
+                localizationService.Providers.Insert(0, xmlProvider);
+            }
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            ProviderBasedLocalizationService localizationService = context.Locate.Advanced.GetInstance<LocalizationService>() as ProviderBasedLocalizationService;
+            if (localizationService != null)
+            {
+                //Gets any provider that has the same name as the one initialized.
+                LocalizationProvider localizationProvider = localizationService.Providers.FirstOrDefault(p => p.Name.Equals(ProviderName, StringComparison.Ordinal));
+                if (localizationProvider != null)
+                {
+                    //If found, remove it.
+                    localizationService.Providers.Remove(localizationProvider);
+                }
+            }
+        }
+    }
+}
