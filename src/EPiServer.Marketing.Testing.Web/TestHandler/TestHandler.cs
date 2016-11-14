2441369c4f48c2ac439d8e309fb0eaafc74be10e
Adam Pitcher <adam.pitcher@episerver.com>
2016-11-14 14:58:40 -0500

    * Removed redundant setting of test data after expiring a cookie. * Added call to evaluatecookie() in loadedchildren events to handle any tests created from there. * removed events parameter as no longer necessary in evaluatecookies()

--- src/EPiServer.Marketing.Testing.Web/TestHandler/TestHandler.cs
+++ src/EPiServer.Marketing.Testing.Web/TestHandler/TestHandler.cs
@@ -175,6 +175,7 @@ namespace EPiServer.Marketing.Testing.Web
                 {
                     try
                     {
+                        EvaluateCookies();
                         // get the test from the cache
                         var activeTest = _testManager.GetActiveTestsByOriginalItemId(content.ContentGuid).FirstOrDefault();
                         if (activeTest != null)
@@ -231,7 +232,7 @@ namespace EPiServer.Marketing.Testing.Web
             {
                 try
                 {
-                    EvaluateCookies(e);
+                    EvaluateCookies();
 
                     // get the test from the cache
                     var activeTest = _testManager.GetActiveTestsByOriginalItemId(e.Content.ContentGuid).FirstOrDefault();
@@ -337,7 +338,7 @@ namespace EPiServer.Marketing.Testing.Web
         /// Analyzes existing cookies and expires / updates any depending on what tests are in the cache.
         /// It is assumed that only tests in the cache are active.
         /// </summary>
-        private void EvaluateCookies(ContentEventArgs e)
+        private void EvaluateCookies()
         {
             if (!DbReadWrite())
             {
@@ -361,11 +362,6 @@ namespace EPiServer.Marketing.Testing.Web
                     // else we have a valid test but the cookie test id doesnt match because user created a new test 
                     // on the same content.
                     _testDataCookieHelper.ExpireTestDataCookie(testCookie);
-
-                    var originalContent = e.Content;
-                    var contentVersion = e.ContentLink.WorkID == 0 ? e.ContentLink.ID : e.ContentLink.WorkID;
-                    TestDataCookie tc = new TestDataCookie();
-                    SetTestData(e.Content, activeTest, tc, contentVersion, out tc, out contentVersion);
                 }
             }
         }
