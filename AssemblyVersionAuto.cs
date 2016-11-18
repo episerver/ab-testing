fb1090d74679d6187ceb9370a0e3fe8271750402
Jason Masterson <jason.masterson@episerver.com>
2016-11-18 14:37:14 -0500

    Added MVC as a dependency in the Nuspec, removed the prerelease dependencies from nuspecs for release.

--- AssemblyVersionAuto.cs
+++ AssemblyVersionAuto.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.5-beta")]
-[assembly: AssemblyInformationalVersion("1.0.5-beta")]
+[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyInformationalVersion("1.0.5")]
