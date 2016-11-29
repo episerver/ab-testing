1df5a56f8b42fef9ab6d96d5b9619ae6f8997698
Jason Masterson <jason.masterson@episerver.com>
2016-11-29 12:01:36 -0500

    Merge fix?

--- src/EPiServer.Marketing.Testing.Web/Helpers/TestingContextHelper.cs
+++ src/EPiServer.Marketing.Testing.Web/Helpers/TestingContextHelper.cs
@@ -122,7 +122,7 @@ namespace EPiServer.Marketing.Testing.Web.Helpers
             // and store in the request so we can use it later
             try
             {
-                var pageHelper = ServiceLocator.Current.GetInstance<IPageRouteHelper>();
+                var pageHelper = ServiceLocator.Current.GetInstance<EPiServer.Web.Routing.IPageRouteHelper>();
                 return pageHelper.Page;
             }
             catch { } // sometimes requests dont contain epi pages.
