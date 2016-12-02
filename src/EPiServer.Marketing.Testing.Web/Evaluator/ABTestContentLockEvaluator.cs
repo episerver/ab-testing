c82a12cda7eb75073468d8ff546dac176c83c31a
Jason Masterson <jason.masterson@episerver.com>
2016-12-02 10:09:38 -0500

    MAR-611: implemented contentlockevaluator interface and initialized it to remove the publish menu for items under an AB Test.

---
+++ src/EPiServer.Marketing.Testing.Web/Evaluator/ABTestContentLockEvaluator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EPiServer.Core;
+using EPiServer.Marketing.Testing.Web.Repositories;
+using EPiServer.ServiceLocation;
+
+namespace EPiServer.Marketing.Testing.Web.Evaluator
+{
+    public class ABTestLockEvaluator : IContentLockEvaluator
+    {
+        private IMarketingTestingWebRepository _webRepo;
+
+        public ABTestLockEvaluator()
+        {
+            _webRepo = ServiceLocator.Current.GetInstance<IMarketingTestingWebRepository>();
+        }
+
+        public ContentLock IsLocked(ContentReference contentLink)
+        {
+            var contentRepo = ServiceLocator.Current.GetInstance<IContentRepository>();
+            var content = contentRepo.Get<IContent>(contentLink);
+
+            if (content == null)
+                return null;
+
+            var test = _webRepo.GetActiveTestForContent(content.ContentGuid);
+
+            if (test == null || test.Id == null || test.Id == Guid.Empty)
+                return null;
+
+            return new ContentLock(contentLink, test.Owner, "ActiveABTestLock", test.CreatedDate);
+        }
+    }
+}
