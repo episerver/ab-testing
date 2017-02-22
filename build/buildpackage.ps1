b92d95c8719616b4475e26ecc580a77ba61825b4
brpe <brian.petrowicz@episerver.com>
2017-02-22 16:05:30 -0500

    v9.24.1 is the last version before 10, so commerce max limit needs to be 11 instead of 10, otherwise it won't install the package.

--- build/buildpackage.ps1
+++ build/buildpackage.ps1
@@ -1,75 +1,51 @@
 $deployDir = $args[0] 
 $projectDir = $args[1]
-#$dbSchema = $args[2]
 $dependency = $args[2]
-#$assemblies = $args[4]
 
 $artifactsDir = "..\artifacts"
-
-#if (Test-Path $deployDir)
-#{
-#    Remove-Item $deployDir -Recurse
-#}
-
-#new-item $deployDir -itemtype directory
-#new-item $deployDir\lib -itemtype directory
-#new-item $deployDir\tools\epiupdates\sql -itemtype directory
-
 $srcDir = "..\src\"
-#$sqlPath = $srcDir + "Database\" + $dbSchema + "\*.sql"
-
-#$projectDir = $srcDir + $projectName + "\"
 $jsonPath = $projectDir + "\project.json"
 $assemblyInfoPath = $projectDir + "\AssemblyVersionAuto.cs"
 
 # loop over dependencies to get the versions for each one
 if ($dependency)
 {
-$depends = $dependency.Split("*")
+	$depends = $dependency.Split("*")
 
-$myJson = Get-Content -Raw -Path $jsonPath | ConvertFrom-Json
-$nugetVersions = ""
+	$myJson = Get-Content -Raw -Path $jsonPath | ConvertFrom-Json
+	$nugetVersions = ""
 
-ForEach($dep in $depends) 
-{
-	$minVersion = $myJson.dependencies.$dep
-	$maxVersionLimit = [int]$minVersion.Split('.')[0] + 1
+	ForEach($dep in $depends) 
+	{
+		$minVersion = $myJson.dependencies.$dep
+		$maxVersionLimit = [int]$minVersion.Split('.')[0] + 1
 
-	$dependencyName = $dep.Replace(".", "")
-	$nugetVersions = $nugetVersions + $dependencyName + "MinVersion=" + $minVersion + ";" + $dependencyName + "MaxLimit=" + $maxVersionLimit + ";"
+		# bit of a hack, since 9.24.1 is the last version before 10, this needs an extra bump
+		if ($dep.equals("EPiServer.Commerce"))
+		{
+			$maxVersionLimit += 1
+		}
+		
+		$dependencyName = $dep.Replace(".", "")
+		$nugetVersions = $nugetVersions + $dependencyName + "MinVersion=" + $minVersion + ";" + $dependencyName + "MaxLimit=" + $maxVersionLimit + ";"
+	}
+
+	# need to wrap entire string in quotes for nuget to be happy
+	$nugetVersions = '"' + $nugetVersions + '"'
 }
 
-$nugetVersions = '"' + $nugetVersions + '"'
-$nugetVersions
-}
 $match = (Select-String -Path $assemblyInfoPath -Pattern 'AssemblyInformationalVersion\("(.+)"\)').Matches[0]
 $assemblyVersion = $match.Groups[1].Value
 
-#$nuspecPath = $projectDir + "Package.nuspec"
-#$projectPath = $projectDir + "" + $projectName + ".xproj"
-
-# loop over this for multiple assemblies
-#$assemblies = $assemblies.Split(";")
-#ForEach($assembly in $assemblies) 
-#{ 
-#    $path = "$artifactsDir\Release\net45\" + $assembly + ".dll"
-#    Copy-Item -Path $path -Destination $deployDir\lib 
-#}
-
-#Copy-Item -Path $nuspecPath -Destination $deployDir
-#Copy-Item -Path $projectPath -Destination $deployDir
-#Copy-Item -Path $sqlPath -Destination $deployDir\tools\epiupdates\sql -Recurse -Force
-
+# go into dir where everything is to run nuget command from there so that a proj or spec file doesn't need to be specified.  Apparently, this is the only way 
+# to pass in multiple versions
 cd $deployDir
 if ($nugetVersions)
 {
 	..\..\..\build\resources\nuget\nuget.exe pack -Build -Properties $nugetVersions -Version $assemblyVersion -Exclude *.xproj
 }
-else
+else # if there are no dependency versions to set, then dont pass in null as it blows nuget up
 {
 	..\..\..\build\resources\nuget\nuget.exe pack -Build -Version $assemblyVersion -Exclude *.xproj
 }
 cd "..\..\..\build"
-
-#Copy-Item  $deployDir\*.nupkg $artifactsDir
-#Remove-Item $deployDir -Recurse
