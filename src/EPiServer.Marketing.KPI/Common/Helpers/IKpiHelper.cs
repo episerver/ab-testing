87794e276d082a24d17c02cf4ca4d7b386be5a8d
brpe <brian.petrowicz@episerver.com>
2017-03-03 09:09:27 -0500

    remove unused usings

--- src/EPiServer.Marketing.KPI/Common/Helpers/IKpiHelper.cs
+++ src/EPiServer.Marketing.KPI/Common/Helpers/IKpiHelper.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace EPiServer.Marketing.KPI.Common.Helpers
+﻿namespace EPiServer.Marketing.KPI.Common.Helpers
 {
     public interface IKpiHelper
     {
