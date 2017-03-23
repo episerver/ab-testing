768d1aa197169927385227ecf1b99faba5b50b95
brpe <brian.petrowicz@episerver.com>
2017-03-23 13:18:20 -0400

    add new migration and script for db changes, update database version and zip for automatic upgrades

---
+++ Models/Testing/Migrations/201703231659456_MergeBranchCleanup.cs
@@ -0,0 +1,18 @@
+namespace Testing.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MergeBranchCleanup : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+          
+        }
+    }
+}
