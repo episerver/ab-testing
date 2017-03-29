f3f108bf6b97b44842768e233ea6ef8403d6042d
Adam <Adam.Pitcher@episerver.com>
2017-03-28 23:09:09 -0400

    * Roughed in weight widget template * CheckMark Image * Roughed stylings.

--- src/EPiServer.Marketing.Testing.Web/ClientResources/widgets/KpiWeightWidget.js
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/widgets/KpiWeightWidget.js
@@ -1,1 +1,27 @@
-﻿
+﻿define([
+    "dojo/_base/declare",
+    "dojo/Evented",
+    "dijit/_WidgetBase",
+    "dijit/_OnDijitClickMixin",
+    "dijit/_TemplatedMixin",
+    "dijit/_WidgetsInTemplateMixin",
+    "dojo/text!./templates/KpiWeightWidget.html",
+    'dojo/dom',
+    'xstyle/css!marketing-testing/css/KpiWidget.css',
+
+
+], function (declare, Evented, _WidgetBase, _OnDijitClickMixin, _TemplatedMixin, _WidgetsInTemplateMixin, template, dom) {
+
+    return declare("KpiWeightWidget", [Evented, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+
+        templateString: template,
+
+        label: "",
+
+        id: this.id,
+
+        postCreate: function () {
+
+        }
+    });
+});
