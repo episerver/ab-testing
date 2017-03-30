20a1ae89ebb6269eec9fb3b56d17df428749abfc
Adam Pitcher <adam.pitcher@episerver.com>
2017-03-30 14:05:36 -0400

    * More refactoring.

--- src/EPiServer.Marketing.Testing.Web/ClientResources/widgets/WeightSelector.js
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/widgets/WeightSelector.js
@@ -29,69 +29,50 @@
 
         _init: function () {
             this._setSelectorValueLabel();
-            this._adjustImportanceSelectors();
+            this._importanceSelected();
         },
 
         _setSelectorValueLabel: function () {
             this.selectorValue.innerHTML = this.value;
         },
 
-        _adjustImportanceSelectors: function () {
-            if (this.value) {
-                switch (this.value) {
+        _importanceSelected: function (evt) {
+            var weight = this.value;
+            if (evt) {
+                weight = evt.srcElement.value;
+            }
+
+            if (weight) {
+
+                this._resetCheckedState();
+                if (this.value != weight) {
+                    this._setValueAttr(weight);
+                }
+                switch (weight) {
                     case "Low":
-                        this._importanceClicked();
                         this.importanceLow.checked = true;
+                        domClass.replace(this.importanceMedium, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight--default");
+                        domClass.replace(this.importanceHigh, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight--default");
                         break;
                     case "Medium":
-                        this._importanceClicked();
                         this.importanceMedium.checked = true;
+                        domClass.replace(this.importanceLow, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight-low");
+                        domClass.replace(this.importanceHigh, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight--default");
                         break;
                     case "High":
-                        this._importanceClicked();
                         this.importanceHigh.checked = true;
+                        domClass.replace(this.importanceLow, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight-low");
+                        domClass.replace(this.importanceMedium, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight-medium");
                         break;
-                    default:
-                        console.debug("KpiWeightWidget init: " + this.value + " is not a valid weight value.");
                 }
             }
         },
 
-        _importanceClicked: function (evt) {
-            var weight = this.value;
-            var me = this;
-            if (evt) {
-                weight = evt.srcElement.value;
-            }
-            switch (weight) {
-                case "Low":
-                    if (me.value && me.value != "Low") {
-                        this._setValueAttr("Low");
-                    }
-                    this.importanceMedium.checked = false;
-                    domClass.replace(this.importanceMedium, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight--default");
-                    this.importanceHigh.checked = false;
-                    domClass.replace(this.importanceHigh, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight--default");
-                    break;
-                case "Medium":
-                    if (this.value && this.value != "Medium") {
-                        this._setValueAttr("Medium");
-                    }
-                    this.importanceLow.checked = false;
-                    domClass.replace(this.importanceLow, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight-low");
-                    this.importanceHigh.checked = false;
-                    domClass.replace(this.importanceHigh, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight--default");
-                    break;
-                case "High":
-                    if (this.value && this.value != "High") {
-                        this._setValueAttr("High");
-                    }
-                    this.importanceLow.checked = false;
-                    domClass.replace(this.importanceLow, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight-low");
-                    this.importanceMedium.checked = false;
-                    domClass.replace(this.importanceMedium, "epi-weightSelector-kpiweight epi-weightSelector-kpiweight-medium");
-                    break;
-            }
+        _resetCheckedState: function () {
+            this.importanceLow.checked = false;
+            this.importanceMedium.checked = false;
+            this.importanceHigh.checked = false;
         }
+
     });
 });
