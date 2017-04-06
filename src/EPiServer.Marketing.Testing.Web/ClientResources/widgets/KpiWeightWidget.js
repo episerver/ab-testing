5ff6cb6371c4c91800a0f70fd037a348bc079cdb
Adam Pitcher <adam.pitcher@episerver.com>
2017-04-06 15:27:23 -0400

    * Fixed really bad merge issue.

--- src/EPiServer.Marketing.Testing.Web/ClientResources/widgets/KpiWeightWidget.js
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/widgets/KpiWeightWidget.js
@@ -3,48 +3,34 @@
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
-    "dojo/text!./templates/KpiSummariesWidget.html",
-    "epi/i18n!marketing-testing/nls/abtesting",
+    "dojo/text!./templates/KpiWeightWidget.html",
     "dojo/dom",
-    "dojo/dom-construct",
-    "marketing-testing/widgets/WeightSelector",
-    "marketing-testing/widgets/KpiSummaryRow",
-    "xstyle/css!marketing-testing/css/KpiWidget.css"
-], function (declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template, resources, dom, domConstruct, weightSelector, kpiSummaryRow) {
-
-    return declare("KpiSummariesWidget", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
-
-        id: this.id,
+    "xstyle/css!marketing-testing/css/KpiWidget.css",
+    "marketing-testing/widgets/WeightSelector"
+], function (declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template, dom) {
+    return declare("KpiWeightWidget", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
 
         templateString: template,
 
-        resources: resources,
+        label: "",
 
-        //requires being set to an array of objects
-        //objects need to contain name, conversion, weight (Low, Medium, High), and performance value
-        kpis: null,
+        id: this.id,
 
-        startup: function () {
-            var me = this;
-            var trInsertNode = dom.byId(this.id + "Summaries")
-            var templatedRow;
-            if (trInsertNode) {
-                trInsertNode.innerHTML = "";
-                for (var x = 0; x < this.kpis.length; x++) {
-                    templatedRow = new KpiSummaryRow({
-                        name: this.kpis[x].name,
-                        conversions: this.kpis[x].conversions,
-                        performance: this.kpis[x].performance
-                    })
+        kpiWidgetId: null,
 
-                    domConstruct.place(templatedRow.domNode, trInsertNode);
-                    new weightSelector({
-                        value: this.kpis[x].weight,
-                        disabled: true,
-                        showLabel: false
-                    }).placeAt(templatedRow.id + "weight");
-                }
-            }
+        _setValueAttr: function (value) {
+            var kpiWidget = dijit.byId(this.kpiWidgetId);
+            kpiWidget._setImportanceAttr(value);
+        },
+
+        postCreate: function () {
+            this.weightSelector._setValueAttr(this.value);
+            var kpiWidget = dijit.byId(this.kpiWidgetId);
+            kpiWidget._setImportanceAttr(this.value);
+        },
+
+        _setWeight: function () {
+            this._setValueAttr(this.weightSelector.value);
         }
     });
 });
