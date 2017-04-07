410d673a9cfcb100de70411722b21150bad07399
Jason Masterson <jason.masterson@episerver.com>
2017-04-07 16:35:33 -0400

    Version bump to 2.2.3

--- src/EPiServer.Marketing.Testing.Web/AssemblyVersionAuto.cs
+++ src/EPiServer.Marketing.Testing.Web/AssemblyVersionAuto.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.2.2")]
-[assembly: AssemblyInformationalVersion("2.2.2")]
+[assembly: AssemblyVersion("2.2.3")]
+[assembly: AssemblyInformationalVersion("2.2.3")]
