3d0a25c1ea1b58a1ab2e742cc77a7c7b35dfa9ad
hemc <hemc@EPUSLPTHEMC>
2017-05-10 09:43:22 -0400

    initial checkin of archived test component, basically copy of the versions component found in cms

---
+++ src/EPiServer.Marketing.Testing.Web/Archived/ArchivedTestComponent.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Shell.ViewComposition;
+
+namespace EPiServer.Marketing.Testing.Web.Archived
+{
+    [Component(
+        //Auto-plugs in the component to the assets panel of cms (See EPiServer.Shell.PlugInArea
+        //in the EPiServer.UI assembly for Dashboard and CMS constants)
+        PlugInAreas = "/episerver/cms/assets",
+        Categories = "cms",
+        WidgetType = "marketing-testing/cmsuicomponents/ArchivedTestComponent",
+        //Define language path to translate Title/Description.
+        LanguagePath = "/abtesting/archivedtestcomponent",
+        Title = "asdf",
+        Description = "asdf")]
+    public class ArchivedTestComponent
+    {
+    }
+}
