de1aaf5518a0d9020efb549d3c57d6a972dba9b9
brpe <brian.petrowicz@episerver.com>
2017-05-15 12:03:44 -0400

    sandcastle didn't like the formatting of this comment for some reason.  now it's happy.

--- src/EPiServer.Marketing.KPI/Common/Attributes/ClientScriptAttribute.cs
+++ src/EPiServer.Marketing.KPI/Common/Attributes/ClientScriptAttribute.cs
@@ -7,10 +7,9 @@ namespace EPiServer.Marketing.KPI.Common.Attributes
     /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class ClientScriptAttribute : Attribute
-    {   
+    {
         /// <summary>
-        /// The resource name, including namespace, containing the javascript necessary
-        /// to process & evaluate the client side KPI conversion conditions.
+        ///  The resource name, including namespace, containing the javascript necessary to process and evaluate the client side KPI conversion conditions.
         /// </summary>
         public string ClientSideEvaluationScript { get; set; }
     }
