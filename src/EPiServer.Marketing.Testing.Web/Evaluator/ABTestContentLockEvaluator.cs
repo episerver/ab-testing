610e0c70a8e0e1851ccca3567acdae516d543e2e
Adam Pitcher <adam.pitcher@episerver.com>
2017-05-30 14:36:10 -0400

    * Modified contenetlock evaluator to lock content based on language as well as guid * Removed MarketingTestMetaDataExtender - legacy solution for content locking and was interfering with unlocking content not under test * updated Unit Tests.

--- src/EPiServer.Marketing.Testing.Web/Evaluator/ABTestContentLockEvaluator.cs
+++ src/EPiServer.Marketing.Testing.Web/Evaluator/ABTestContentLockEvaluator.cs
@@ -3,6 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 using EPiServer.Core;
 using EPiServer.Marketing.Testing.Web.Repositories;
 using EPiServer.ServiceLocation;
+using EPiServer.Marketing.Testing.Web.Helpers;
 
 namespace EPiServer.Marketing.Testing.Web.Evaluator
 {
@@ -10,6 +11,7 @@ namespace EPiServer.Marketing.Testing.Web.Evaluator
     {
         private IMarketingTestingWebRepository _webRepo;
         private IContentRepository _contentRepo;
+        private IEpiserverHelper _episerverHelper;
         internal string _abLockId = "ActiveABTestLock";
 
         [ExcludeFromCodeCoverage]
@@ -17,12 +19,14 @@ namespace EPiServer.Marketing.Testing.Web.Evaluator
         {
             _webRepo = ServiceLocator.Current.GetInstance<IMarketingTestingWebRepository>();
             _contentRepo = ServiceLocator.Current.GetInstance<IContentRepository>();
+            _episerverHelper = ServiceLocator.Current.GetInstance<IEpiserverHelper>();
         }
 
-        internal ABTestLockEvaluator(IMarketingTestingWebRepository theWebRepo, IContentRepository theContentRepo)
+        internal ABTestLockEvaluator(IMarketingTestingWebRepository theWebRepo, IContentRepository theContentRepo, IEpiserverHelper theEpiHelper)
         {
             _webRepo = theWebRepo;
             _contentRepo = theContentRepo;
+            _episerverHelper = theEpiHelper;
         }
 
         public ContentLock IsLocked(ContentReference contentLink)
@@ -32,7 +36,7 @@ namespace EPiServer.Marketing.Testing.Web.Evaluator
             if (content == null)
                 return null;
 
-            var test = _webRepo.GetActiveTestForContent(content.ContentGuid);
+            var test = _webRepo.GetActiveTestForContent(content.ContentGuid, _episerverHelper.GetContentCultureinfo());
 
             if (test == null || test.Id == null || test.Id == Guid.Empty)
                 return null;
