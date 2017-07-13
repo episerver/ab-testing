2e27ac5163613b9bb2dcebba1fe73587c0997d27
Adam Pitcher <adam.pitcher@episerver.com>
2017-07-13 12:37:12 -0400

    Helper for resolving URL

---
+++ src/EPiServer.Marketing.KPI/Common/Helpers/IKpiEpiserverHelper.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPiServer.Marketing.KPI.Common.Helpers
+{
+    interface IKpiEpiserverHelper
+    {
+        /// <summary>
+        /// Leverages UrlResolver to return the url of content associated with the supplied content reference        /// 
+        /// </summary>
+        /// <param name="contentReference"></param>
+        /// <returns></returns>
+        string GetUrl(ContentReference contentReference);
+    }
+}
---
+++ src/EPiServer.Marketing.KPI/Common/Helpers/KpiEpiserverHelper.cs
@@ -0,0 +1,15 @@
+﻿using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
+
+namespace EPiServer.Marketing.KPI.Common.Helpers
+{
+    [ServiceConfiguration(ServiceType = typeof(IKpiEpiserverHelper), Lifecycle = ServiceInstanceScope.Singleton)]
+    public class KpiEpiserverHelper : IKpiEpiserverHelper
+    {
+        public string GetUrl(ContentReference contentReference)
+        {
+            return UrlResolver.Current.GetUrl(contentReference);
+        }
+    }
+}
