253fe3ad45a7e9cb557edce97a9f29d750cb5cf8
Jason Masterson <jason.masterson@episerver.com>
2017-07-17 09:50:51 -0400

    Update to package version

--- src/EPiServer.Marketing.Testing.Web/AssemblyVersionAuto.cs
+++ src/EPiServer.Marketing.Testing.Web/AssemblyVersionAuto.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyInformationalVersion("2.4.0")]
+[assembly: AssemblyVersion("2.4.1")]
+[assembly: AssemblyInformationalVersion("2.4.1")]
