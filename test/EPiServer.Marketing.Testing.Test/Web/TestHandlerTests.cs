597261a01b7bd7bf261129eb29beab1c2c7a3999
Adam Pitcher <adam.pitcher@episerver.com>
2017-07-18 14:23:22 -0400

    * Fixed unit tests

--- test/EPiServer.Marketing.Testing.Test/Web/TestHandlerTests.cs
+++ test/EPiServer.Marketing.Testing.Test/Web/TestHandlerTests.cs
@@ -190,30 +190,7 @@ namespace EPiServer.Marketing.Testing.Test.Web
             Assert.Equal(content.ContentLink, args.ContentLink);
         }
 
-        [Fact]
-        public void TestHandler_System_With_Valid_TestData_Cookie_And_No_Active_Test_Calls_Expire_Cookie()
-        {
-            var content = new BasicContent();
-            content.ContentGuid = _noAssociatedTestGuid;
-            content.ContentLink = new ContentReference();
-
-            var testHandler = GetUnitUnderTest();
-
-            _mockMarketingTestingWebRepository.Setup(call => call.GetActiveTestsByOriginalItemId(It.IsAny<Guid>(),It.IsAny<CultureInfo>())).Returns(new List<IMarketingTest>());
-            _mockTestDataCookieHelper.Setup(call => call.GetTestDataFromCookie(It.IsAny<string>(), It.IsAny<string>())).Returns(new TestDataCookie { Converted = false, ShowVariant = true, Viewed = false });
-            _mockTestDataCookieHelper.Setup(call => call.GetTestDataFromCookies()).Returns(new List<TestDataCookie>() { new TestDataCookie() });
-            _mockTestDataCookieHelper.Setup(call => call.IsTestParticipant(It.IsAny<TestDataCookie>())).Returns(true);
-            _mockTestDataCookieHelper.Setup(call => call.HasTestData(It.IsAny<TestDataCookie>())).Returns(true);
-            _mockContextHelper.Setup(call => call.SwapDisabled(It.IsAny<ContentEventArgs>())).Returns(false);
-            _mockContextHelper.Setup(call => call.GetCurrentPage()).Returns(new BasicContent());
-            _mockContextHelper.Setup(call => call.IsRequestedContent(It.IsAny<IContent>()))
-                .Returns(true);
-            _mockEpiserverHelper.Setup(call => call.GetContentCultureinfo()).Returns(new CultureInfo("en-GB"));
-
-            ContentEventArgs args = new ContentEventArgs(content);
-            testHandler.LoadedContent(new object(), args);
-            _mockTestDataCookieHelper.Verify(call => call.ExpireTestDataCookie(It.IsAny<TestDataCookie>()), Times.Once(), "System should have called ExpireTestDataCookie, but did not");
-        }
+       
 
         [Fact]
         public void TestHandler_Disabling_The_Page_Swap_Returns_The_Published_Page()
