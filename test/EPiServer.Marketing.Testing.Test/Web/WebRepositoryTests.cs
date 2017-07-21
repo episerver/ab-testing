9a1ea5f3e4c2b49bad2f7ec4762c6d06fa7f98a8
Adam <Adam@DESKTOP-SRGQV36>
2017-07-21 11:17:55 -0400

    * Removed Base for web tests (did not provide the amount of benefit first thought)

--- test/EPiServer.Marketing.Testing.Test/Web/WebRepositoryTests.cs
+++ test/EPiServer.Marketing.Testing.Test/Web/WebRepositoryTests.cs
@@ -14,18 +14,45 @@ using EPiServer.Marketing.Testing.Core.DataClass;
 using EPiServer.Marketing.Testing.Core.DataClass.Enums;
 using EPiServer.Marketing.Testing.Core.Manager;
 using EPiServer.Marketing.KPI.Manager.DataClass;
-
+using EPiServer.ServiceLocation;
+using EPiServer.Marketing.Testing.Web.Helpers;
+using EPiServer.Marketing.KPI.Common.Helpers;
 
 namespace EPiServer.Marketing.Testing.Test.Web
 {
-    public class WebRepositoryTests : WebTestBase
+    public class WebRepositoryTests
     {
+        internal Mock<ITestManager> _mockTestManager;
+        internal Mock<ILogger> _mockLogger;
+        internal Mock<IServiceLocator> _mockServiceLocator;
+        internal Mock<ITestResultHelper> _mockTestResultHelper;
+        internal Mock<IMarketingTestingWebRepository> _mockMarketingTestingWebRepository;
+        internal Mock<IKpiManager> _mockKpiManager;
+        internal Mock<IHttpContextHelper> _mockHttpHelper;
+        internal Mock<IEpiserverHelper> _mockEpiserverHelper;
+        internal Mock<IKpiHelper> _mockKpiHelper = new Mock<IKpiHelper>();
         private Guid _testGuid = Guid.Parse("984ae93a-3abc-469f-8664-250328ce8220");
 
         private MarketingTestingWebRepository GetUnitUnderTest()
         {
             _mockLogger = new Mock<ILogger>();
-            var aRepo = new MarketingTestingWebRepository(InitializeMocks().Object, _mockLogger.Object);
+            _mockServiceLocator = new Mock<IServiceLocator>();
+            _mockTestResultHelper = new Mock<ITestResultHelper>();
+            _mockHttpHelper = new Mock<IHttpContextHelper>();
+            _mockKpiManager = new Mock<IKpiManager>();
+            _mockMarketingTestingWebRepository = new Mock<IMarketingTestingWebRepository>();
+            _mockEpiserverHelper = new Mock<IEpiserverHelper>();
+            _mockTestManager = new Mock<ITestManager>();
+
+            _mockServiceLocator.Setup(sl => sl.GetInstance<ITestManager>()).Returns(_mockTestManager.Object);
+            _mockServiceLocator.Setup(sl => sl.GetInstance<IKpiHelper>()).Returns(_mockKpiHelper.Object);
+            _mockServiceLocator.Setup(sl => sl.GetInstance<IKpiManager>()).Returns(_mockKpiManager.Object);
+            _mockServiceLocator.Setup(call => call.GetInstance<ITestResultHelper>()).Returns(_mockTestResultHelper.Object);
+            _mockServiceLocator.Setup(call => call.GetInstance<IMarketingTestingWebRepository>()).Returns(_mockMarketingTestingWebRepository.Object);
+            _mockServiceLocator.Setup(call => call.GetInstance<IHttpContextHelper>()).Returns(_mockHttpHelper.Object);
+            _mockServiceLocator.Setup(call => call.GetInstance<IEpiserverHelper>()).Returns(_mockEpiserverHelper.Object);
+
+            var aRepo = new MarketingTestingWebRepository(_mockServiceLocator.Object, _mockLogger.Object);
             return aRepo;
         }
 
