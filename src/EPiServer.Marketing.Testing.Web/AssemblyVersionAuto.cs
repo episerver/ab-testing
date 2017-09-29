f76903b661591c88324b31766da7b228db07874c
Jason Masterson <jason.masterson@episerver.com>
2017-09-29 15:03:49 -0400

    version changes to reflect the actual features going in. Reverted back to a bugfix for the testing solution and incremented the feature number for the commerce KPI package to support commerce 11 packages.

--- src/EPiServer.Marketing.Testing.Web/AssemblyVersionAuto.cs
+++ src/EPiServer.Marketing.Testing.Web/AssemblyVersionAuto.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.5.0")]
-[assembly: AssemblyInformationalVersion("2.5.0")]
+[assembly: AssemblyVersion("2.4.3")]
+[assembly: AssemblyInformationalVersion("2.4.3")]
