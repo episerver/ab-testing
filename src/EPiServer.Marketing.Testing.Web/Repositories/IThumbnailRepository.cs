b810e0d6511a6a56fd18d3b83db41912c27119d2
Adam Pitcher <Adam.Pitcher@episerver.com>
2017-10-11 15:15:58 -0400

    Added thumbnailstore to the intialize module Created thumbnail repository for launching phantom, creating and deleting file and getting random filename Created thumbnail repo interface Added servicelocation to repository Added thumbnailstore which handles get (create and return file name) and delete (removing the file after it is no longer needed). reduced wait limit on waitforexit with phantom process to allow for files to be created.

---
+++ src/EPiServer.Marketing.Testing.Web/Repositories/IThumbnailRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace EPiServer.Marketing.Testing.Web.Repositories
+{
+    interface IThumbnailRepository
+    {
+        string getRandomFileName();
+        Process getCaptureProcess(string page, string fileName, string user, string pass);
+        ActionResult deleteCaptureFile(string fileName);
+    }
+}
