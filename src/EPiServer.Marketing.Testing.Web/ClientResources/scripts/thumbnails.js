08c8cbc7941b9fd94f9e360a563b4422c05fe131
Adam Pitcher <Adam.Pitcher@episerver.com>
2019-02-08 07:36:38 -0500

    Added code to leverage RasterizeHTML when encountering a browser other than IE.    Will continue to use Phantom solutoin with IE.

--- src/EPiServer.Marketing.Testing.Web/ClientResources/scripts/thumbnails.js
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/scripts/thumbnails.js
@@ -1,31 +1,60 @@
 ﻿define([
     "epi/dependency",
+    'marketing-testing/scripts/rasterizeHTML'
 ],
-function (dependency) {
-    return {
-        _setThumbnail: function (canvasId, url) {
-            var me = this;
-            this._setThumbState(canvasId, "block", "none", "none");
-            this.thumbstore = this.thumbstore || dependency.resolve("epi.storeregistry").get("marketing.thumbnailstore");
-            this.thumbstore.get(url.replace(/\//g, "$")).then(function (result) {
-                var thumbnail = new Image();
+    function (dependency, rasterizehtml) {
+        return {
+            _setThumbnail: function (canvasId, url) {
+                var me = this;
+                this._setThumbState(canvasId, "block", "none", "none");
+
+                if (this._isBrowserIE()) {
+                    this._renderServersideThumbnail(canvasId, url);
+                } else {
+                    this._renderClientsideThumbnail(canvasId, url);
+                }
+            },
+
+            _isBrowserIE: function () {
+                return navigator.appName === 'Microsoft Internet Explorer'
+                    || !!(navigator.userAgent.match(/Trident/)
+                        || navigator.userAgent.match(/rv:11/))
+                    || (typeof $.browser !== "undefined" && $.browser.msie === 1);
+            },
+
+            _renderServersideThumbnail: function (canvasId, url) {
+                var me = this;
+                console.log("Generating Thumbnail (S)");
+                this.thumbstore = this.thumbstore || dependency.resolve("epi.storeregistry").get("marketing.thumbnailstore");
+                this.thumbstore.get(url.replace(/\//g, "$")).then(function (result) {
+                    canvasId.height = 768;
+                    canvasId.width = 1024;
+                    var thumbnail = new Image();
+                    thumbnail.src = "data:image/png;base64," + result;
+                    thumbnail.onload = function () {
+                        var context = canvasId.getContext('2d');
+                        context.drawImage(thumbnail, 0, 0);
+                        me._setThumbState(canvasId, "none", "block", "none");
+                    };
+                }).otherwise(function () {
+                    me._setThumbState(canvasId, "none", "none", "block");
+                });
+            },
+
+            _renderClientsideThumbnail: function (canvasId, url) {
+                var me = this;
+                console.log("Generating Thumbnail (C)");
                 canvasId.height = 768;
                 canvasId.width = 1024;
-                thumbnail.src = "data:image/png;base64," + result;
-                thumbnail.onload = function () {
-                    var context = canvasId.getContext('2d');
-                    context.drawImage(thumbnail, 0, 0);
+                rasterizehtml.drawURL(url, canvasId, { height: 768, width: 1024 }).then(function success(renderResult) {
                     me._setThumbState(canvasId, "none", "block", "none");
-                }
-            }).otherwise(function () {
-                me._setThumbState(canvasId, "none", "none", "block");
-            });
-        },
+                });
+            },
 
-        _setThumbState: function (canvasId, spinnerState, previewState, errorState) {
-            document.getElementById(canvasId.id + "-spinner").style.display = spinnerState;
-            canvasId.style.display = previewState;
-            document.getElementById(canvasId.id + "-error").style.display = errorState;
-        }
-    }
-});
+            _setThumbState: function (canvasId, spinnerState, previewState, errorState) {
+                document.getElementById(canvasId.id + "-spinner").style.display = spinnerState;
+                canvasId.style.display = previewState;
+                document.getElementById(canvasId.id + "-error").style.display = errorState;
+            }
+        };
+    });
