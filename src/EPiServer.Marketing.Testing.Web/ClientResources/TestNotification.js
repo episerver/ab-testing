b6b8af0cbd25dd20ef849bb0db8661055dcec784
Sai Venkat Thumala <Saivenkat.Thumala@episerver.com>
2020-04-08 11:05:08 -0400

    Update label to reflect the toggle state of AB feature.

--- src/EPiServer.Marketing.Testing.Web/ClientResources/TestNotification.js
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/TestNotification.js
@@ -12,94 +12,101 @@ define([
     "epi/i18n!marketing-testing/nls/abtesting"
 ],
 
-function (
-    declare,
-    event,
-    Stateful,
-    domConstruct,
-    on,
-    topic,
-    Destroyable,
-    datetime,
-    dependency,
-    ContentActionSupport,
-    resources
-) {
+    function (
+        declare,
+        event,
+        Stateful,
+        domConstruct,
+        on,
+        topic,
+        Destroyable,
+        datetime,
+        dependency,
+        ContentActionSupport,
+        resources
+    ) {
 
-    return declare([Stateful, Destroyable], {
-        // summary:
-        //      Shows the notification when active or finished test exists for the current content.
-        // tags:
-        //      public
+        return declare([Stateful, Destroyable], {
+            // summary:
+            //      Shows the notification when active or finished test exists for the current content.
+            // tags:
+            //      public
 
-        constructor: function (params) {
-        },
+            isAbEnabled: null,
+            constructor: function (params) {
+            },
 
-        _contentActionSupport: ContentActionSupport,
+            _contentActionSupport: ContentActionSupport,
 
-        postscript: function () {
-            this._storeName = "marketing.abtesting";
-            this.inherited(arguments);
-        },
+            postscript: function () {
+                this._storeName = "marketing.abtesting";
+                this._configStoreName = "marketing.abtestingconfig";
+                this.inherited(arguments);
+            },
 
-        _valueSetter: function (value) {
-            var me = this;
-            this.value = value;
-            this._store = this._store || dependency.resolve("epi.storeregistry").get(this._storeName);
+            _valueSetter: function (value) {
+                var me = this;
+                this.value = value;
+                this._store = this._store || dependency.resolve("epi.storeregistry").get(this._storeName);
+                var configStore = this.configStore || dependency.resolve("epi.storeregistry").get(this._configStoreName);
+                configStore.get().then(function (configValues) {
+                    me.isAbEnabled = configValues.isEnabled;
+                    me._store.get(value.contentData.contentGuid).then(function (test) {
+                        // Hide notification for content without a pending or active test
+                        if (!test || !test.title || test.state == 3 || !test.id) {
+                            me.set("notification", null);
+                            return;
+                        }
 
-            this._store.get(value.contentData.contentGuid).then(function (test) {
-                // Hide notification for content without a pending or active test
-                if (!test || !test.title || test.state == 3 || !test.id) {
-                    me.set("notification", null);
-                    return;
+                        var notificationMessage = me._constructNotificationMessage(test, me.isAbEnabled);
+                        me.set("notification", {
+                            content: notificationMessage,
+                            commands: []
+                        });
+                    });
+                });
+            },
+
+            _constructNotificationMessage: function (test, isAbEnabled) {
+                var message = resources.notificationbar.ongoing_test;
+                var testLinkText = resources.notificationbar.details_link_text;
+                var testLinkTooltip = resources.notificationbar.details_link_tooltip;
+                var page = "details";
+
+                // Inactive (scheduled)
+                if (test.state === 0) {
+                    message = resources.notificationbar.scheduled_test +
+                        datetime.toUserFriendlyString(test.startDate, null, false, true) + ". ";
                 }
 
-                var notificationMessage = me._constructNotificationMessage(test);
-                me.set("notification", {
-                    content: notificationMessage,
-                    commands: []
-                });
-            });
-        },
+                // Done, finished
+                if (test.state === 2) {
+                    message = resources.notificationbar.completed_test;
+                    testLinkText = resources.notificationbar.winner_link_text;
+                    testLinkTooltip = resources.notificationbar.winner_link_tooltip;
+                    page = "pickwinner";
+                }
 
-        _constructNotificationMessage: function (test) {
-            var message = resources.notificationbar.ongoing_test;
-            var testLinkText = resources.notificationbar.details_link_text;
-            var testLinkTooltip = resources.notificationbar.details_link_tooltip;
-            var page = "details";
+                if (!isAbEnabled) {
+                    message = resources.detailsview.test_disabled + "&nbsp";
+                }
 
-             // Inactive (scheduled)
-            if (test.state == 0) {
-                message = resources.notificationbar.scheduled_test + 
-                    datetime.toUserFriendlyString(test.startDate, null, false, true) + ". ";
-            }
-            
-             // Done, finished
-            if (test.state == 2) {
-                message = resources.notificationbar.completed_test;
-                var testLinkText = resources.notificationbar.winner_link_text;
-                var testLinkTooltip = resources.notificationbar.winner_link_tooltip;
-                page = "pickwinner";
-            }
+                var notificationMesage = domConstruct.create("div", { innerHTML: message });
 
-            var notificationMesage = domConstruct.create("div", { innerHTML: message });
-            
-            if(test.state !== 2 || this._contentActionSupport.hasAccess(this.value.contentData.accessMask, this._contentActionSupport.accessLevel.Publish))
-            {
-                var testLink =  domConstruct.create("a", { href: "#", innerHTML: testLinkText, title: testLinkTooltip }, notificationMesage);
-            
-            this.own(
-                on(testLink, "click", function (e) {
-                    event.stop(e);
-                    topic.publish("/epi/shell/context/request", {
-                        uri: "epi.marketing.testing:///testid=" + test.id + "/" + page
-                    }, {sender: this});
-                })
-            );
+                if (test.state !== 2 || this._contentActionSupport.hasAccess(this.value.contentData.accessMask, this._contentActionSupport.accessLevel.Publish)) {
+                    var testLink = domConstruct.create("a", { href: "#", innerHTML: testLinkText, title: testLinkTooltip }, notificationMesage);
+
+                    this.own(
+                        on(testLink, "click", function (e) {
+                            event.stop(e);
+                            topic.publish("/epi/shell/context/request", {
+                                uri: "epi.marketing.testing:///testid=" + test.id + "/" + page
+                            }, { sender: this });
+                        })
+                    );
+                }
+
+                return notificationMesage;
             }
-            
-            return notificationMesage;
-        }
+        });
     });
-
-});
