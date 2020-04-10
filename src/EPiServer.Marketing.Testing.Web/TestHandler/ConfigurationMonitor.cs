a60c0d019a4a811f4a5aaa7530331692efaa552c
Sai Venkat Thumala <Saivenkat.Thumala@episerver.com>
2020-04-10 13:49:05 -0400

    Added ConfigurationMonitor.

---
+++ src/EPiServer.Marketing.Testing.Web/TestHandler/ConfigurationMonitor.cs
@@ -0,0 +1,38 @@
+﻿using EPiServer.Marketing.Testing.Web.Config;
+using EPiServer.ServiceLocation;
+
+namespace EPiServer.Marketing.Testing.Web
+{
+    /// <summary>
+    /// Contains methods to monitor configuration changes and apply them to all the remote nodes.
+    /// </summary>
+    public class ConfigurationMonitor : IConfigurationMonitor
+    {
+        private IServiceLocator serviceLocator;
+
+        /// <summary>
+        /// Default
+        /// </summary>
+        /// <param name="serviceLocator"></param>
+        public ConfigurationMonitor(IServiceLocator serviceLocator)
+        {
+            this.serviceLocator = serviceLocator;
+        }
+
+        /// <summary>
+        /// Enables or disables AB testing based on config changes.
+        /// </summary>
+        public void HandleConfigurationChange()
+        {
+            var testHandler = serviceLocator.GetInstance<ITestHandler>();
+            if (AdminConfigTestSettings.Current.IsEnabled)
+            {
+                testHandler.EnableABTesting();
+            }
+            else
+            {
+                testHandler.DisableABTesting();
+            }
+        }
+    }
+}
---
+++ test/EPiServer.Marketing.Testing.Test/Web/ConfigurationMonitorTests.cs
@@ -0,0 +1,54 @@
+﻿using EPiServer.Marketing.Testing.Web;
+using EPiServer.Marketing.Testing.Web.Config;
+using EPiServer.ServiceLocation;
+using Moq;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace EPiServer.Marketing.Testing.Test.Web
+{
+    [ExcludeFromCodeCoverage]
+    public class ConfigurationMonitorTests
+    {
+        private Mock<ITestHandler> mockTestHandler;
+        private Mock<IServiceLocator> mockServiceLocator;
+
+        public ConfigurationMonitor GetUnitUnderTest()
+        {
+            mockTestHandler = new Mock<ITestHandler>();
+            mockTestHandler.Setup(t => t.EnableABTesting()).Verifiable();
+            mockTestHandler.Setup(t => t.DisableABTesting()).Verifiable();
+
+            mockServiceLocator = new Mock<IServiceLocator>();
+            mockServiceLocator.Setup(sl => sl.GetInstance<ITestHandler>()).Returns(mockTestHandler.Object);
+
+            return new ConfigurationMonitor(mockServiceLocator.Object);
+        }
+
+        [Fact]
+        public void HandleConfigurationChange_EnablesAB_When_EnabledInConfig()
+        {
+            var configMonitor = GetUnitUnderTest();
+
+            AdminConfigTestSettings._currentSettings = new AdminConfigTestSettings() { IsEnabled = true };
+
+            configMonitor.HandleConfigurationChange();
+
+            mockTestHandler.Verify(t => t.EnableABTesting(), Times.Once);
+            mockTestHandler.Verify(t => t.DisableABTesting(), Times.Never);
+        }
+
+        [Fact]
+        public void HandleConfigurationChange_DisablesAB_When_DisabledInConfig()
+        {
+            var configMonitor = GetUnitUnderTest();
+
+            AdminConfigTestSettings._currentSettings = new AdminConfigTestSettings() { IsEnabled = false };
+
+            configMonitor.HandleConfigurationChange();
+
+            mockTestHandler.Verify(t => t.DisableABTesting(), Times.Once);
+            mockTestHandler.Verify(t => t.EnableABTesting(), Times.Never);
+        }
+    }
+}
