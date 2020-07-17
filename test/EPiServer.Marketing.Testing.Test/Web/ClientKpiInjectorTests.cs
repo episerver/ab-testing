0095ebc667913ec2b66235f79563c8ef6b4117a6
Dustin Sier <Dustin.Sier@episerver.com>
2020-07-17 11:06:36 -0400

    Addd assert to make sure the BOM char is in the client KPI we're using to test if client KPIs get BOMs removed.

--- test/EPiServer.Marketing.Testing.Test/Web/ClientKpiInjectorTests.cs
+++ test/EPiServer.Marketing.Testing.Test/Web/ClientKpiInjectorTests.cs
@@ -8,6 +8,7 @@ using EPiServer.Marketing.Testing.Web.ClientKPI;
 using EPiServer.Marketing.Testing.Web.Helpers;
 using EPiServer.Marketing.Testing.Web.Repositories;
 using EPiServer.ServiceLocation;
+using Mediachase.Web.Console.Config;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -210,7 +211,9 @@ namespace EPiServer.Marketing.Testing.Test.Web
 
             aClientKpiInjector.AppendClientKpiScript();
 
-            //verify that the response was added to the stream
+            Assert.Contains(Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble()), aClientKpi.ClientEvaluationScript);
+
+            //verify that the response was added to the stream and does not contain the BOM char
             mockHttpContextHelper.Verify(hch => hch.SetResponseFilter(It.Is<ABResponseFilter>(abrf => 
                                                 !abrf.clientScript.Contains(Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble())))),
                                                 Times.Once(), "the clientScript contains a UTF Preamble");
